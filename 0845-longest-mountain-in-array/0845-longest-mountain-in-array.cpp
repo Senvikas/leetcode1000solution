5746586c5b22d110cffe10771d88ae9528566552
Senvikas <111661736+Senvikas@users.noreply.github.com>
2022-10-31 01:00:46 +0530

    Time: 55 ms (22.10%), Space: 18.5 MB (89.37%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -1,55 +1,44 @@
-class Solution {
-public:
-    int longestMountain(vector<int>& arr) {
-        int n = arr.size();
-        int low = 0;
-        int high = 0;
-        int maxlen = 0;
-        
-        string s = "";
-        for(int i=0; i<n-1; i++)
+class Solution
+{
+    public:
+        int longestMountain(vector<int> &arr)
         {
-            if(arr[i] > arr[i+1])
-                s += '>';
-            else if(arr[i] < arr[i+1])
-                s += '<';
-            else
-                s += '=';
-        }
-        for(int i=0; i<n-1; i++){
-            cout<<s[i];
-        }cout<<endl;
-        
-        for(int i=0; i<n; i++){
-            //small is found
-            if(s[i] == '<')
+            int n = arr.size();
+            int low = 0;
+            int high = 0;
+            int maxlen = 0;
+
+            for (int i = 0; i < n-1; i++)
             {
-                if(high != 0)
+               	//small is found
+                if(arr[i] < arr[i+1])                         //if (s[i] == '<')
                 {
+                    if (high != 0)
+                    {
+                        high = 0;
+                        low = 1;
+                    }
+                    else
+                    {
+                        low++;
+                    }
+                }
+                //greater element found
+                else if (arr[i] > arr[i + 1])                                     //else if(s[i] == '>')
+                {
+                    if (low != 0)
+                    {
+                        high++;
+                        maxlen = max(maxlen, high + low + 1);
+                    }
+                }
+                else // = found
+                {
+                    low = 0;
                     high = 0;
-                    low = 1;
-                }
-                else
-                {
-                    low++;
                 }
             }
-            else if(s[i] == '>')
-            {
-                if(low != 0)
-                {
-                    high++;
-                    maxlen = max(maxlen, high+low+1);
-                }
-            }
-            else{              // = found
-                // maxlen = max(maxlen, high+low);
-                low = 0;
-                high = 0;
-            }
+
+            return maxlen;
         }
-        
-        
-        return maxlen;
-    }
 };
