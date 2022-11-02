d7bf174b6397da285343b18423c06dde1ee06a9f
Senvikas <111661736+Senvikas@users.noreply.github.com>
2022-11-02 17:36:15 +0530

    Time: 90 ms (33.06%), Space: 13.2 MB (78.09%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
@@ -10,13 +11,51 @@ public:
             for(int j=0; j<n; j++)
             {
                 pq.push(matrix[i][j]);
+                if(pq.size() > k)
+                    pq.pop();
+
             }
             
         }
-       while(pq.size() != k)
-       {
-           pq.pop();
-       }
-        return pq.top();
+     return pq.top();
+    }
+};
+
+*/
+    
+    class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k)
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int midval;
+        int startval = matrix[0][0];
+        int endval = matrix[m-1][n-1];
+        
+        while(startval <= endval)
+        {
+            midval = startval + (endval - startval)/2;
+            int smallcount = 0;
+            for(int i=0; i<m; i++)
+            {
+                //binary search on each row
+                int low = 0;
+                int high = n-1;
+                int mid;
+                
+                while(low<=high)
+                {
+                    mid = low + (high - low)/2;
+                    if(matrix[i][mid] <= midval) low = mid+1;
+                    else high = mid-1;
+                }
+                smallcount += low;
+            }
+            if(smallcount < k) startval = midval+1;
+            else endval = midval - 1; 
+        }
+        return startval;
     }
 };
