3aa8f06a13cf1106512e635a5e1d5c83a5797ebf
Senvikas <111661736+Senvikas@users.noreply.github.com>
2022-11-02 16:41:28 +0530

    Time: 145 ms (79.97%), Space: 14 MB (77.37%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,7 +1,5 @@
 class Solution
 {
-    set<string> st;
-
     public:
         int ladderLength(string b, string e, vector<string> &wordList)
         {
@@ -9,14 +7,12 @@ class Solution
             for (auto &w: wordList)
                 valid.insert(w);
             if (valid.find(e) == valid.end()) return 0;
-            unordered_set<string> vis;
             int n = b.length();
 
             int cnt = 1;
             queue<string> q;
             q.push(b);
-            vis.insert(b);
-            vis.erase(b);
+            valid.erase(b);
 
            	//bfs
             while (!q.empty())
@@ -27,23 +23,22 @@ class Solution
                     string s = q.front();
                     q.pop();
 
-                        for (int i = 0; i < n; i++)
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (char c = 'a'; c <= 'z'; c++)
                         {
-                                for (char c='a'; c<='z'; c++)
-                                {
-                                    string newword = s;
-                                    newword[i] = c;
+                            string newword = s;
+                            newword[i] = c;
 
-                                    if (newword == e)
-                                        return cnt + 1;
-                                    if ( valid.find(newword) != valid.end())
-                                    {
-                                       //vis.insert(newword);
-                                        q.push(newword);
-                                        valid.erase(newword);
-                                    }
-                                }
+                            if (newword == e)
+                                return cnt + 1;
+                            if (valid.find(newword) != valid.end())
+                            {
+                                q.push(newword);
+                                valid.erase(newword);
+                            }
                         }
+                    }
                 }
                 cnt++;
             }
