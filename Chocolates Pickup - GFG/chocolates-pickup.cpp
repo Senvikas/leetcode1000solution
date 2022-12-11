d399bb809e1dd5aafa356e3fabac57fc008d0433
Senvikas <111661736+Senvikas@users.noreply.github.com>
2022-12-11 20:03:42 +0530

    Added solution - LeetHub

--- Chocolates Pickup - GFG/chocolates-pickup.cpp
+++ Chocolates Pickup - GFG/chocolates-pickup.cpp
@@ -35,8 +35,51 @@ class Solution {
   }
   
     int solve(int n, int m, vector<vector<int>>& grid) {
-        vector<vector<vector<int> > > dp(n, vector<vector<int>>(m, vector<int>(m, -1)));           
-        return f(n, m, grid, 0, 0, m-1, dp);
+        vector<vector<vector<int> > > dp(n, vector<vector<int>>(m, vector<int>(m, 0)));           
+        //return f(n, m, grid, 0, 0, m-1, dp);
+        //memo
+        //filling up the last 2d vec
+        for(int j1=0; j1<m; j1++)
+        {
+            for(int j2=0; j2<m; j2++)
+            {
+                if(j1 == j2)
+                dp[n-1][j1][j2] = grid[n-1][j1];
+                else
+                dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+
+        //filling up further from 1 - n
+        for(int i=n-2; i>=0; i--)
+        {
+            for(int j1=0; j1<m; j1++)
+            {
+                for(int j2=0; j2<m; j2++)
+                {
+                    int m_c = 0;
+                  for(int dj1=-1; dj1<2; dj1++)
+                  {
+                      for(int dj2=-1; dj2<2; dj2++)
+                      {
+                          int choc = 0;
+                          if(j1==j2) 
+                            choc = grid[i][j1];
+                          else
+                            choc = grid[i][j1] + grid[i][j2];
+                            
+                        if(j1+dj1>=0 && j1+dj1<m && j2+dj2>=0 && j2+dj2<m )
+                        choc += dp[i+1][j1+dj1][j2+dj2];
+                        else
+                        choc += -1e7;
+                        m_c = max(m_c, choc);
+                      }
+                  }
+                  dp[i][j1][j2] = m_c;
+                }
+            }
+        }
+        return dp[0][0][m-1];
     }
 };
 
