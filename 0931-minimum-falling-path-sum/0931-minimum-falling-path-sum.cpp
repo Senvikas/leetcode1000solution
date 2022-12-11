3912299b0f6529aa4570dbc536c6e3bbdb7c7807
Senvikas <111661736+Senvikas@users.noreply.github.com>
2022-12-11 11:57:37 +0530

    Time: 12 ms (97.34%), Space: 10.3 MB (54.21%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,25 +1,46 @@
-class Solution {
-public:
-    int solve(vector<vector<int>>& arr, int n, int i, int j, vector<vector<int>>& dp)
-    {
-        if(j<0 || j>=n) return 1e8;
-        if(i==n-1) return arr[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        int d = arr[i][j] + solve(arr, n, i+1, j, dp);
-        int dl = arr[i][j] + solve(arr, n, i+1, j-1, dp);
-        int dr = arr[i][j] + solve(arr, n, i+1, j+1, dp);
-        
-        return dp[i][j] = min(d, min(dl, dr));
-    }
-    
-    int minFallingPathSum(vector<vector<int>>& arr) {
-        int n = arr[0].size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        int ans = INT_MAX;
-        for(int j=0; j<n; j++)
+class Solution
+{
+    public:
+        int solve(vector<vector < int>> &arr, int n, int i, int j, vector< vector< int>> &dp)
         {
-            ans = min(ans, solve(arr, n, 0, j, dp));
+            if (j < 0 || j >= n) return 1e8;
+            if (i == n - 1) return arr[i][j];
+            if (dp[i][j] != -1) return dp[i][j];
+            int d = arr[i][j] + solve(arr, n, i + 1, j, dp);
+            int dl = arr[i][j] + solve(arr, n, i + 1, j - 1, dp);
+            int dr = arr[i][j] + solve(arr, n, i + 1, j + 1, dp);
+
+            return dp[i][j] = min(d, min(dl, dr));
         }
-        return ans;
+
+    int minFallingPathSum(vector<vector < int>> &arr)
+    {
+        int n = arr[0].size();
+        /*   vector<vector < int>> dp(n, vector < int>(n, -1));
+            int ans = INT_MAX;
+            for(int j=0; j < n; j++)
+            {
+                ans = min(ans, solve(arr, n, 0, j, dp));
+            }
+            return ans;         */
+        vector<vector < int>> dp(n, vector<int> (n, 0));
+        int ans = INT_MAX;
+       	//filling up the first row
+        for (int j = 0; j < n; j++)
+            dp[0][j] = arr[0][j];
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                int d = arr[i][j] + dp[i - 1][j];
+                int dl = arr[i][j] + (j - 1 >= 0 ? dp[i - 1][j - 1] : 1e6);
+                int dr = arr[i][j] + (j + 1 <= n - 1 ? dp[i - 1][j + 1] : 1e6);
+
+                dp[i][j] = min(d, min(dl, dr));
+            }
+        }
+
+        return* min_element(dp[n - 1].begin(), dp[n - 1].end());
     }
 };
