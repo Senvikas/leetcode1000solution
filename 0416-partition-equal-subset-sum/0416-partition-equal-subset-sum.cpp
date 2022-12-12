5ab96c16160a6d6ceb602f1c3e3db66a0550d639
Senvikas <111661736+Senvikas@users.noreply.github.com>
2022-12-12 13:24:01 +0530

    Time: 494 ms (44.89%), Space: 9.8 MB (83.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -22,7 +22,38 @@ class Solution
         for (auto &x: arr) t_sum += x;
         cout << t_sum << endl;
         if (t_sum & 1) return false;
-        vector<vector < int>> dp(201, vector<int> (t_sum / 2 + 1, -1));
-        return solve(arr, t_sum / 2, n - 1, dp);
+        int k = t_sum / 2 + 1;
+        /*    vector<vector < int>> dp(201, vector < int>(k,-1));
+               return solve(arr, k-1, n - 1, dp);            */
+
+        /*     vector<vector < bool>> dp(201, vector<bool> (k, 0));
+               for (int i = 0; i < n; i++) dp[i][0] = true;
+               if (arr[0] <= k) dp[0][arr[0]] = true;
+
+               for (int ind = 1; ind < n; ind++)
+               {
+                   for (int tar = 1; tar < k; tar++)
+                   {
+                       bool exc = dp[ind - 1][tar];
+                       bool inc = false;
+                       if (tar >= arr[ind]) inc = dp[ind - 1][tar - arr[ind]];
+
+                      	//update the dp
+                       dp[ind][tar] = exc || inc;
+                   }
+               }
+               return dp[n - 1][k - 1];           */
+
+       	//further space optimization
+        vector<bool> prev(k, false), curr(k, false);
+        prev[0] = true;
+        if (arr[0] < k) prev[arr[0]] = true;
+        for (int ind = 1; ind < n; ind++)
+        {
+            for (int tar = 1; tar < k; tar++)
+                curr[tar] = prev[tar] | ((tar >= arr[ind]) ? prev[tar - arr[ind]] : false);
+            prev = curr;
+        }
+        return prev[k - 1];
     }
 };
