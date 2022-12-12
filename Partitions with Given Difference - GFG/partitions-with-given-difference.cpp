4f064945d2d150c61bd923aa61c93681dacd0579
Senvikas <111661736+Senvikas@users.noreply.github.com>
2022-12-13 00:28:48 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
@@ -13,23 +13,24 @@ class Solution {
         if(t-d <0 || (t-d)&1) return 0;
         int sum = (t-d)/2;
         
-        vector<vector<int>>dp(n, vector<int>(sum+1, 0));
-        if(arr[0] == 0) dp[0][0] = 2;
-        else dp[0][0] = 1;
+        vector<int>prev(sum+1, 0), curr(sum+1, 0);
+        if(arr[0] == 0) prev[0] = 2;
+        else prev[0] = 1;
         
-        if(arr[0] != 0 && arr[0] <= sum) dp[0][arr[0]] = 1;
+        if(arr[0] != 0 && arr[0] <= sum) prev[arr[0]] = 1;
         for(int i=1; i<n; i++)
         {
             for(int s=0; s<=sum; s++)
             {
-                int exc = dp[i-1][s];
+                int exc = prev[s];
                 int inc = 0;
-                if(s>=arr[i]) inc = dp[i-1][s-arr[i]];
+                if(s>=arr[i]) inc = prev[s-arr[i]];
                 
-                dp[i][s] = (inc + exc)%mod;
+                curr[s] = (inc + exc)%mod;
             }
+            prev = curr;
         }
-        return dp[n-1][sum];
+        return prev[sum];
     }
 };
 
