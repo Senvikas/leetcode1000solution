b820765058dfa07cd41b0ba4a02192d5f28e4c69
Senvikas <111661736+Senvikas@users.noreply.github.com>
2022-12-13 17:23:37 +0530

    Time: 15 ms (86.27%), Space: 9.4 MB (59.51%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,49 @@
+class Solution
+{
+    public:
+        int countPartitions(int n, int d, vector<int> &arr)  //refer to dp 18.
+        {
+            int t = 0;
+            for (auto &i: arr) t += i;
+            if (t - d < 0 || (t - d) &1) return 0;
+            int sum = (t - d) / 2;
+
+            vector<int> prev(sum + 1, 0), curr(sum + 1, 0);
+            if (arr[0] == 0) prev[0] = 2;
+            else prev[0] = 1;
+
+            if (arr[0] != 0 && arr[0] <= sum) prev[arr[0]] = 1;
+            for (int i = 1; i < n; i++)
+            {
+                for (int s = 0; s <= sum; s++)
+                {
+                    int exc = prev[s];
+                    int inc = 0;
+                    if (s >= arr[i]) inc = prev[s - arr[i]];
+
+                    curr[s] = (inc + exc) ;
+                }
+                prev = curr;
+            }
+            return prev[sum];
+        }
+
+    int solve(vector<int> &arr, int t, int i, vector<vector< int>> &dp)
+    {
+        if (i < 0) return t == 0;
+        if (dp[i][2000 + t] != -1) return dp[i][2000 + t];
+        int sub = solve(arr, t - arr[i], i - 1, dp);
+        int add = solve(arr, t + arr[i], i - 1, dp);
+
+        return dp[i][2000 + t] = add + sub;
+    }
+
+    int findTargetSumWays(vector<int> &arr, int target)
+    {
+        int n = arr.size();
+        /*      vector<vector<int>>dp(n+1, vector < int>(4001, -1));
+               return solve(arr, target, arr.size()-1, dp);          */
+
+        return countPartitions(n, target, arr );
+    }
+};
