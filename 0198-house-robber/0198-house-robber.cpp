93877ad31970bb39df70c4449f4e6b4fc5e28e10
Senvikas <111661736+Senvikas@users.noreply.github.com>
2022-12-14 10:30:09 +0530

    Time: 3 ms (56.21%), Space: 7.8 MB (50.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,32 +1,35 @@
-class Solution {
-public:
-int solve(vector<int> &nums, int n){
-   
-    int prev1 = nums[0];
-    int prev2 = 0;
-    
-    for(int i=1; i<n; i++)
+class Solution
+{
+    public:
+        int solve(int ind, vector<int> &arr, vector<int> &dp)
+        {
+            if (ind == 0) return arr[ind];
+            if (ind < 0) return 0;
+            if (dp[ind] != -1) return dp[ind];
+            int exc = solve(ind - 1, arr, dp);
+            int inc = arr[ind] + solve(ind - 2, arr, dp);
+
+            return dp[ind] = max(inc, exc);
+        }
+
+    int rob(vector<int> &arr)
     {
-        int inc = prev2 + nums[i];
-        int exc = prev1;
-        int ans = max(inc, exc);
-        prev2 = prev1;
-        prev1 = ans;
-    }
-    
-    return prev1;
-}
-    
-    
-    
-    
-    
-    int rob(vector<int>& nums) {
-        int n=nums.size();
-        
-        
-        
-        int ans = solve(nums, n);
-        return ans;
+        int n = arr.size();
+       	//if (n == 2) return max(arr[0], arr[1]);
+        /*     vector<int> dp(n + 1, -1);
+                return solve(n - 1, arr, dp);              */
+
+       	//tabulation
+        vector<int> dp(n, 0);
+        dp[0] = arr[0];
+        if (n > 1)
+            dp[1] = max(arr[0], arr[1]);
+        for (int ind = 2; ind < n; ind++)
+        {
+            int exc = dp[ind - 1];
+            int inc = arr[ind] + dp[ind - 2];
+            dp[ind] = max(exc, inc);
+        }
+        return dp[n - 1];
     }
 };
