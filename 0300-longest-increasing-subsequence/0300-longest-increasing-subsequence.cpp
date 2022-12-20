e278a3847ab1091cd7017e2e99bc1c315419e4c4
Senvikas <111661736+Senvikas@users.noreply.github.com>
2022-12-20 12:49:18 +0530

    Time: 381 ms (58.26%), Space: 10.4 MB (90.57%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -24,6 +24,8 @@ class Solution
         vector<vector < int>> dp(n, vector<int> (n + 1, -1));
         return f(0, -1, arr, n, dp);
         */
+
+        /*   	//memoization
         vector<vector < int>> dp(n + 1, vector<int> (n + 1, 0));
         for (int ind = n - 1; ind >= 0; ind--)
         {
@@ -37,5 +39,41 @@ class Solution
             }
         }
         return dp[0][0];
+
+        */
+
+        /*
+       	//space optimizatoin
+        vector<int> next_row(n + 1, 0), curr_row(n + 1, 0);
+        for (int ind = n - 1; ind >= 0; ind--)
+        {
+            for (int prev_ind = ind - 1; prev_ind >= -1; prev_ind--)
+            {
+                int len = next_row[prev_ind + 1];
+                if (prev_ind == -1 || arr[ind] > arr[prev_ind])
+                    len = max(len, 1 + next_row[ind + 1]);
+
+                curr_row[prev_ind + 1] = len;
+            }
+            next_row = curr_row;
+        }
+        return curr_row[0];
+        */
+
+       	//intuitve
+        vector<int> dp(n, 1);
+        int ans = 1;
+        for (int ind = 0; ind < n; ind++)
+        {
+            for (int prev_ind = 0; prev_ind < ind; prev_ind++)
+            {
+                if (arr[ind] > arr[prev_ind])
+                {
+                    dp[ind] = max(dp[ind], 1 + dp[prev_ind]);
+                }
+            }
+            ans = max(ans, dp[ind]);
+        }
+        return ans;
     }
 };
