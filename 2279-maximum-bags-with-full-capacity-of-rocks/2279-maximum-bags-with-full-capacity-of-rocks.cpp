3d5dda1668c52d968c5d8edecf8166fc5453efee
Senvikas <111661736+Senvikas@users.noreply.github.com>
2022-12-27 12:59:52 +0530

    Time: 906 ms (5.05%), Space: 94.2 MB (6.59%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+    static bool cmp(pair<int, int> &a, pair<int, int> &b)
+    {
+        return a.first - a.second < b.first - b.second;
+    }
+    public:
+        int maximumBags(vector<int> &capacity, vector<int> &rocks, int additionalRocks)
+        {
+
+            int n = capacity.size();
+           	//otherwise use greedy:
+            vector<pair<int, int>> capRoc;
+            for (int i = 0; i < n; i++)
+                capRoc.push_back({ capacity[i],
+                    rocks[i] });
+
+           	//now sort and put the least empty bag at first position
+            sort(capRoc.begin(), capRoc.end(), cmp);
+            for (auto p: capRoc)
+            {
+                cout << p.first << " " << p.second << endl;
+            }
+            int cnt = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int diff = capRoc[i].first - capRoc[i].second;
+                if (diff == 0 || diff <= additionalRocks)
+                {
+                    additionalRocks -= diff;
+                    cnt++;
+                }
+                else break;
+            }
+
+            return cnt;
+        }
+};
