c85dc8b21d84c73d8a39cf615eb9fe35699bd7e5
Senvikas <111661736+Senvikas@users.noreply.github.com>
2022-12-29 18:01:44 +0530

    Time: 199 ms (60.77%), Space: 15.4 MB (44.03%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,47 +1,41 @@
 class Solution
 {
     public:
-        int ladderLength(string b, string e, vector<string> &wordList)
+        int ladderLength(string b, string e, vector<string> &wl)
         {
-            unordered_set<string> valid;
-            for (auto &w: wordList)
-                valid.insert(w);
-            if (valid.find(e) == valid.end()) return 0;
-            int n = b.length();
-
-            int cnt = 1;
+            unordered_set<string> st(wl.begin(), wl.end());
+            unordered_set<string> vis;
             queue<string> q;
+            if (st.find(e) == st.end()) return 0;
+            int n = b.length();
+            int cnt = 1;
             q.push(b);
-            valid.erase(b);
+            vis.insert(b);
 
-           	//bfs
             while (!q.empty())
             {
                 int size = q.size();
                 while (size--)
                 {
-                    string s = q.front();
+                    auto w = q.front();
                     q.pop();
-
                     for (int i = 0; i < n; i++)
                     {
                         for (char c = 'a'; c <= 'z'; c++)
                         {
-                            string newword = s;
-                            newword[i] = c;
-
-                            if (newword == e)
-                                return cnt + 1;
-                            if (valid.find(newword) != valid.end())
+                            auto nw = w;
+                            nw[i] = c;
+                            if (nw == e) return cnt + 1;
+                            else if (st.find(nw) != st.end() && vis.find(nw) == vis.end())
                             {
-                                q.push(newword);
-                                valid.erase(newword);
+                                vis.insert(nw);
+                                q.push(nw);
                             }
                         }
                     }
                 }
                 cnt++;
             }
-            return 0;
+            return 0; //in case no possible ladder to rech from b to e.
         }
 };
