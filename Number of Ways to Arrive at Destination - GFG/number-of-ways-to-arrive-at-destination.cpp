e32436c87dbbfc7eaa62e7aa1df868d03b85b609
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-01-12 17:25:03 +0530

    Added solution - LeetHub

--- Number of Ways to Arrive at Destination - GFG/number-of-ways-to-arrive-at-destination.cpp
+++ Number of Ways to Arrive at Destination - GFG/number-of-ways-to-arrive-at-destination.cpp
@@ -7,46 +7,81 @@ using namespace std;
 
 class Solution {
   public:
-    int countPaths(int n, vector<vector<int>>& roads) {
-        priority_queue< pair<int,int>, vector<pair<int,int>>,
-        greater<pair<int,int>> >pq;
-        pq.push({0, 0});
+    int countPaths(int num_of_city, vector<vector<int>>& roads) {
+    // Create a priority queue to store the time and node
+    priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> >pq;
+    
+    // Push the starting node with time 0
+    pq.push({0, 0});
+
+    // Declare a vector 'num_of_ways' to store the number of ways to reach each node
+    vector<int>num_of_ways(num_of_city, 0);
+    
+    // Declare a vector 'min_time' to store the minimum time to reach each node
+    vector<int>min_time(num_of_city, 1e9);
+    
+    // Set the initial time to 0
+    min_time[0] = 0;
+    
+    // Set the initial number of ways to 1
+    num_of_ways[0] = 1;
+
+    // Declare a vector of pairs 'adj' to store the adjacent nodes and their time
+    vector<pair<int,int>>adj[num_of_city];
+    
+    // Store the roads in the adjacency list
+    for(auto edge: roads)
+    {
+        adj[edge[0]].push_back({edge[1], edge[2]});
+        adj[edge[1]].push_back({edge[0], edge[2]});
+    }
+    
+    // Declare a variable 'mod_value' to store the mod value
+    int mod_value = (int)(1e9+7);
+    
+    // Perform Dijkstra's algorithm
+    while(!pq.empty())
+    {
+        // Get the front of the priority queue
+        auto it = pq.top(); pq.pop();
         
-        vector<int>ways(n, 0), dist(n, 1e9);
-        dist[0] = 0;
-        ways[0] = 1;
+        // Get the node
+        int node = it.second;
         
-        vector<pair<int,int>>adj[n];
-        for(auto edge: roads)
+        // Get the time
+        int time = it.first;
+
+        // Iterate through the adjacent nodes
+        for(auto iter: adj[node])
         {
-            adj[edge[0]].push_back({edge[1], edge[2]});
-            adj[edge[1]].push_back({edge[0], edge[2]});
-        }
-        int mod = (int)(1e9+7);
-        while(!pq.empty())
-        {
-            auto it = pq.top(); pq.pop();
-            int node = it.second;
-            int dis = it.first;
+            // Get the adjacent node
+            int adjnode = iter.first;
             
-            for(auto iter: adj[node])
+            // Get the time
+            int time_to_adjnode = iter.second;
+
+            // If the new time is less than the current minimum time to reach the adjacent node
+            if(time + time_to_adjnode < min_time[adjnode])
             {
-                int adjnode = iter.first;
-                int edw = iter.second;
+                // Update the number of ways to reach the adjacent node
+                num_of_ways[adjnode] = num_of_ways[node];
                 
-                if(dis + edw < dist[adjnode])
-                {
-                    ways[adjnode] = ways[node];
-                    dist[adjnode] = dis + edw;
-                    pq.push({dis+edw, adjnode});
-                }
-                else if(dis + edw == dist[adjnode])
-                {
-                    ways[adjnode] = (ways[adjnode] + ways[node])%mod;
+                // Update the minimum time
+                min_time[adjnode] = time + time_to_adjnode;
+                
+                // Push the adjacent node and new time to the priority queue
+                pq.push({time+time_to_adjnode, adjnode});
+            }
+            // If the new time is equal to the current minimum time to reach the adjacent node
+            else if(time + time_to_adjnode == min_time[adjnode])
+            {
+                // Update the number of ways to reach the adjacent node
+                num_of_ways[adjnode] = (num_of_ways[adjnode]+num_of_ways[node])%mod_value;
+
                 }
             }
         }
-        return ways[n-1]%mod;
+        return num_of_ways[num_of_city-1]%mod_value;
     }
 };
 
