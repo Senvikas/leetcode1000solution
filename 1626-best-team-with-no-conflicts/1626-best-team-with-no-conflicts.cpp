9e34f125d4188e4b297d7a219ddf7df919eb1a1a
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-01-31 21:51:01 +0530

    Time: 195 ms (71.43%), Space: 19.9 MB (72.40%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+        int bestTeamScore(vector<int> &scores, vector<int> &ages)
+        {
+            int n = scores.size();
+            vector<pair<int, int>> v;
+            for (int i = 0; i < n; i++)
+            {
+                v.push_back({ ages[i],
+                    scores[i] });
+            }
+            sort(v.begin(), v.end());
+            vector<int> dp(n, 0);
+            dp[0] = v[0].second;
+            int ans = dp[0];
+            for (int i = 1; i < n; i++)
+            {
+                dp[i] = v[i].second;
+                for (int j = 0; j < i; j++)
+                {
+                    if (v[i].second >= v[j].second)
+                    {
+                        dp[i] = max(dp[i], dp[j] + v[i].second);
+                    }
+                }
+                ans = max(ans, dp[i]);
+            }
+            return ans;
+        }
+};
