4c451024171e8a5fe3dda4a2716f41411de78743
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-02-22 01:06:09 +0530

    Time: 36 ms (25.60%), Space: 22.3 MB (93.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,20 +1,8 @@
-class Solution
-{
-    public:
-        int singleNonDuplicate(vector<int> &nums)
-        {
-           	//if mid is even find in right part else in left part
-            int n = nums.size();
-            int left = 0, right = nums.size() - 1;
-            while (left < right)
-            {
-                int mid = (left + right) / 2;
-                if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
-                    left = mid + 1;
-                else
-                    right = mid;
-            }
-
-            return nums[left];
-        }
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int ans = 0;
+        for (int i : nums) ans ^= i;
+        return ans;
+    }
 };
