03bf13fef27f0ddae08a4d7eb48825485fa3f03e
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-04-04 22:03:04 +0530

    Time: 40 ms (24.12%), Space: 42.9 MB (75.16%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    public int partitionString(String s) {
+        int idx = 0;
+        int count = 0;
+        Map<Character, Boolean> mp = new HashMap<>();
+        while (idx < s.length()) {
+            if (mp.containsKey(s.charAt(idx))) {
+                count++; // increment count
+                mp.clear();
+            }
+            mp.put(s.charAt(idx), true);
+            idx++;
+        }
+        return count + 1;
+    }
+}
