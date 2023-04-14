0c3bfc2a5425552ae552e8705a8b986eb1093d1e
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-04-14 20:46:43 +0530

    Time: 276 ms (10.11%), Space: 73.1 MB (36.29%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution
 {
-    public:   //approach -> the longest common subseq. of s and rev(s) will be palindrome
+    public:	//approach -> the longest common subseq. of s and rev(s) will be palindrome
         int lcs(string s, string t, int n, int m)
         {
             vector<vector < int>> dp(n + 1, vector<int> (m + 1, 0));
@@ -10,13 +10,14 @@ class Solution
                 for (int j = 1; j <= m; j++)
                 {
                     if (s[i - 1] == t[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
-                    else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
             return dp[n][m];
         }
 
-    int longestPalindromeSubseq(string s) {
+    int longestPalindromeSubseq(string s)
+    {
         string t = s;
         reverse(s.begin(), s.end());
         int n = s.length(), m = n;
