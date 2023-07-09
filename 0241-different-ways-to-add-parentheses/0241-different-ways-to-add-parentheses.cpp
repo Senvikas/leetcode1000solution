47d1d311224c7e0fa3a643364a6993bf8a5f3fee
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-07-09 21:07:25 +0530

    Time: 3 ms (75.28%), Space: 11.2 MB (45.80%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,33 +1,33 @@
-class Solution
-{
-    vector<int> solve(string s)
-    {
+class Solution {
+public:
+    vector<int> solve(string expression){
         vector<int> ans;
-
-        for (int i = 0; i < s.size(); i++)
-        {
-            if (s[i] == '+' || s[i] == '*' || s[i] == '-')
-            {
-                vector<int> ans1 = solve(s.substr(0, i));
-                vector<int> ans2 = solve(s.substr(i + 1));
-
-                for (int j = 0; j < ans1.size(); j++)
-                {
-                    for (int k = 0; k < ans2.size(); k++)
-                    {
-                        if (s[i] == '-') ans.push_back(ans1[j] - ans2[k]);
-                        else if (s[i] == '+') ans.push_back(ans1[j] + ans2[k]);
-                        else ans.push_back(ans1[j] *ans2[k]);
+        for(int i=0; i<expression.length(); i++){
+            if(expression[i] == '+' || expression[i] == '-' ||
+              expression[i] == '*'){
+                vector<int> ans1 = solve(expression.substr(0,i));
+                vector<int> ans2 = solve(expression.substr(i+1));
+                
+                for(int k=0; k<ans1.size(); k++){
+                    for(int j=0; j<ans2.size(); j++){
+                        if(expression[i] == '+') 
+                            ans.push_back(ans1[k]+ans2[j]);
+                        else if(expression[i] == '-') 
+                            ans.push_back(ans1[k]-ans2[j]);
+                        else if(expression[i] == '*') 
+                            ans.push_back(ans1[k]*ans2[j]); 
                     }
                 }
             }
         }
-        if (ans.size() == 0) ans.push_back(stoi(s));
+        
+        // Base case: If no operator is found, it is a single number
+        if(ans.empty()) ans.push_back(stoi(expression));
+        
         return ans;
     }
-    public:
-        vector<int> diffWaysToCompute(string expression)
-        {
-            return solve(expression);
-        }
+    
+    vector<int> diffWaysToCompute(string expression) {
+       return solve(expression); 
+    }
 };
