95aced26db8fa1d47634fe0a5ce2b78fe62c63d1
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-08-01 08:54:13 +0530

    Added solution - LeetHub

---
+++ Find the number of islands - GFG/find-the-number-of-islands.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            char[][] grid = new char[n][m];
+            for (int i = 0; i < n; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                for (int j = 0; j < m; j++) {
+                    grid[i][j] = S[j].charAt(0);
+                }
+            }
+            Solution obj = new Solution();
+            int ans = obj.numIslands(grid);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+class Pair{
+    int first;
+    int second;
+    
+    public Pair(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+}
+class Solution {
+    private void bfs(int ro, int co, int[][] vis, char[][] grid){
+        vis[ro][co] = 1;
+        Queue<Pair> q = new LinkedList<Pair>();
+        q.add(new Pair(ro, co));
+        int n = grid.length;
+        int m = grid[0].length;
+        
+        while(!q.isEmpty()){
+            int row = q.peek().first;
+            int col = q.peek().second;
+            q.remove();
+            
+            for(int i=-1; i<=1; i++){
+                for(int j=-1; j<=1; j++){
+                    int nrow = row + i;
+                    int ncol = col + j;
+                    
+                    if(nrow>=0 && nrow < n && ncol >= 0 && ncol < m &&
+                        vis[nrow][ncol] == 0 && grid[nrow][ncol] == '1'){
+                            vis[nrow][ncol] = 1;
+                            q.add(new Pair(nrow, ncol));
+                        }
+                }
+            }
+        }
+        return;
+    }
+    // Function to find the number of islands.
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        
+        int[][] vis = new int[n][m];
+        int cnt = 0;
+        for(int row=0; row<n; row++){
+            for(int col=0; col<m; col++){
+                if(vis[row][col] == 0 && grid[row][col] == '1'){
+                    cnt++;
+                    bfs(row, col, vis, grid);
+                }
+            }
+        }
+        return cnt;
+    }
+}
