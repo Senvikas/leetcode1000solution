768c5d4c397147bf5426e53ba5c2f9ea905042c1
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-08-10 22:38:19 +0530

    Time: 3 ms (91.34%), Space: 14 MB (50.05%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,54 +1,13 @@
-class Solution
-{
-    public:
-        int pivot(vector<int> &arr, int n)
-        {
-            for (int i = 1; i < n - 1; i++)
-            {
-                if (arr[i] >= arr[i - 1] && arr[i] > arr[i + 1])
-                    return i;
-            }
-            return 0;
-        }
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        auto ans = find(nums.begin() , nums.end() , target);
 
-    bool search(vector<int> &arr, int target)
-    {
-       	//algo- 1. find pivot, 2. apply bs in both the part
-        int n = arr.size();
-        int pivot_index = pivot(arr, n);
-        if (arr[pivot_index] == target) return true;
-
-        cout << pivot_index;
-       	//for first part 0-k
-        int s = 0;
-        while (pivot_index >= 1 && arr[pivot_index] == arr[pivot_index - 1])
-            pivot_index--;
-        int e = pivot_index;
-        while (s <= e)
+        if(ans!= nums.end())
         {
-            int mid = s + (e - s) / 2;
-            if (arr[mid] == target)
-                return true;
-            if (arr[mid] < target)
-                s = mid + 1;
-            else
-                e = mid - 1;
+            return true;
         }
-       	//second part k+1, n
-        while (pivot_index + 1 < n && arr[pivot_index] == arr[pivot_index + 1])
-            pivot_index++;
-        s = pivot_index + 1, e = n - 1;
-        while (s <= e)
-        {
-            int mid = s + (e - s) / 2;
-            if (arr[mid] == target)
-                return true;
-            if (arr[mid] < target)
-                s = mid + 1;
-            else
-                e = mid - 1;
-        }
-
-        return 0;
+        else
+        return false;
     }
 };
