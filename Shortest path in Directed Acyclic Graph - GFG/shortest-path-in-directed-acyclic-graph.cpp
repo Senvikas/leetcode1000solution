ede86dfb9f67efeaa142558dcff8bc3a4e3e1dbc
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-09-04 22:14:34 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -7,73 +7,79 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 class Solution {
-  public:
-  void topoSort(int node, vector<pair<int,int>>adj[], vector<int>&vis, stack<int>&st)
-  {
-      vis[node] = 1;
-      
-      for(auto it: adj[node])
-      {
-          int v = it.first;
-          if(!vis[v])
-          {
-              topoSort(v, adj, vis, st);
-          }
-      }
-      
-      st.push(node);
-      return ;
-  }
-  
-  
-     vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
-         //step 1: make adj list
-      vector<pair<int,int>> adj[N];
-      for(int i=0; i<M; i++)
-      {
-          int u = edges[i][0];
-          int v = edges[i][1];
-          int wt = edges[i][2];
-          
-          adj[u].push_back({v, wt});
-      }
-      
-      //step 2: do the toposort
-      vector<int>vis(N, 0);
-      stack<int>st;
-      for(int i=0; i<N; i++)
-      {
-          if(!vis[i])
-          {
-              topoSort(i, adj, vis, st);
-          }
-      }
-      
-      //compute the distances
-      vector<int> dist(N, 1e9);
-      dist[0] = 0;
-      while(!st.empty())
-      {
-          int node = st.top();
-          st.pop();
-          
-          for(auto it: adj[node])
-          {
-              int v = it.first;
-              int wt = it.second;
-              if(dist[node] + wt < dist[v])
-              {
-                  dist[v] = wt + dist[node];
-              }
-          }
-      }
-      for(int i=0; i<N; i++)
-      {
-          if(dist[i] >= 1e9) dist[i] = -1;
+private:
+    vector<int> topoSort(int n, vector<pair<int, int>> adj[]) {
+        vector<int> indegree(n, 0);
+
+        // Calculate in-degrees for each node
+        for (int i = 0; i < n; i++) {
+            for (auto it : adj[i]) {
+                indegree[it.first]++;
+            }
+        }
+
+        queue<int> q;
+
+        // Initialize the queue with nodes having in-degree 0
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        vector<int> topo;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            // Reduce in-degree of adjacent nodes
+            for (auto adjnode : adj[node]) {
+                indegree[adjnode.first]--;
+
+                // If in-degree becomes 0, add to the queue
+                if (indegree[adjnode.first] == 0)
+                    q.push(adjnode.first);
+            }
+        }
+
+        return topo;
+    }
+
+public:
+    vector<int> shortestPath(int N, int M, vector<vector<int>>& edges) {
+        vector<pair<int, int>> adj[N];
+        for (int i = 0; i < M; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int wt = edges[i][2];
+
+            adj[u].push_back({ v, wt });
+        }
+
+        vector<int> topo = topoSort(N, adj);
+        vector<int> dist(N, 1e9); // Initialize with a large value
+        dist[0] = 0; // Initialize the source node distance to 0
+
+        // Bellman-Ford algorithm
+        for (int i = 0; i < N; i++) {
+            int node = topo[i];
+            if (dist[node] == INT_MAX) continue; // Skip unreachable nodes
+            for (auto adjnode : adj[node]) {
+                int v = adjnode.first;
+                int wt = adjnode.second;
+
+                if (dist[node] + wt < dist[v])
+                    dist[v] = dist[node] + wt;
+            }
+        }
+        for (int i = 0; i < N; i++) {
+        if (dist[i] == 1e9) dist[i] = -1;
       }
       return dist;
     }
 };
+
 
 
 //{ Driver Code Starts.
