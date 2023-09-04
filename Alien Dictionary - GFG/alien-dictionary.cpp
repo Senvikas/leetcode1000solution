8d470556c48fdc80fa0bc6b36afd8307ee1a7788
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-09-04 09:47:28 +0530

    Added solution - LeetHub

--- Alien Dictionary - GFG/alien-dictionary.cpp
+++ Alien Dictionary - GFG/alien-dictionary.cpp
@@ -8,65 +8,56 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-    public:
-    vector<int> topoSort(int n, vector<int> adj[]) 
-	{
-	    // Kahn's algo
-	    vector<int> indegree(n, 0);
-	    for(int i=0; i<n; i++)
-	    {
-	        for(auto it: adj[i])
-	            indegree[it]++;
-	    }
-	    
-	    queue<int>q;
-	    for(int i=0; i<n; i++)
-	    {
-	        if(indegree[i] == 0) q.push(i);
-	    }
-	    vector<int>ans;
-	    while(!q.empty())
-	    {
-	        int node = q.front();
-	        ans.push_back(node);
-	        q.pop();
-	        
-	        for(auto it: adj[node])
-	        {
-	            indegree[it]--;
-	            if(indegree[it] == 0) q.push(it);
-	        }
-	        
-	    }
-	    return ans;
-	}
-    
-    
-    string findOrder(string dict[], int n, int k) {
-        vector<int>adj[k];
-        for(int i=0; i<n-1; i++)
-        {
-           string s1 = dict[i];
-           string s2 = dict[i+1];
-           
-           int len = min(s1.length(), s2.length());
-           for(int ptr = 0; ptr < len; ptr++)
-           {
-               if(s1[ptr] != s2[ptr])
-               {
-                   adj[s1[ptr]-'a'].push_back(s2[ptr]-'a');
-                   break;
+    private:
+        vector<int> topoSort(int V, vector<int>adj[]){
+            int indegree[V] = {0};
+            for(int i=0; i<V; i++){
+                for(auto it: adj[i]){
+                    indegree[it]++;
+                }
+            }
+            queue<int> q;
+            for(int i=0; i<V; i++){
+                if(indegree[i] == 0)
+                q.push(i);
+            }
+            vector<int> topo;
+            while(!q.empty()){
+               int node = q.front();
+               q.pop();
+               topo.push_back(node);
+               for(auto adjnode : adj[node]){
+                   indegree[adjnode]--;
+                   if(indegree[adjnode] == 0)
+                    q.push(adjnode);
                }
-           }
+            }
+            return topo;
         }
+    public:
+    string findOrder(string dict[], int N, int K) {
         
-        auto ans = topoSort(k, adj);
-        string res = "";
-        for(auto it: ans)
-        {
-            res = res + char(it + 'a');
+        //create DG by defferentiation factor:
+        vector<int> adj[K];
+        for(int i=0; i<N-1; i++){
+            string s1 = dict[i];
+            string s2 = dict[i+1];
+            
+            int len = min(s1.size(), s2.size());
+            for(int j=0; j<len; j++){
+                if(s1[j] != s2[j]){
+                    adj[s1[j]-'a'].push_back(s2[j]-'a');
+                    break;
+                }
+            }
         }
-        return res;
+        vector<int> topo = topoSort(K, adj);
+        string ans = "";
+        for(int i=0; i<K; i++)
+        {
+            ans += char(topo[i] + 'a');
+        }
+        return ans;
     }
 };
 
