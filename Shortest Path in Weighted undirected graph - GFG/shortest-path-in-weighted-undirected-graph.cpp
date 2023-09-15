72a7df5d0770ee7f9e30ef113de896896595aaa6
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-09-16 00:45:52 +0530

    Added solution - LeetHub

--- Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.cpp
+++ Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.cpp
@@ -6,46 +6,45 @@ using namespace std;
 class Solution {
   public:
     vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
-        vector< pair<int,int> >adj[n+1];
-        for(auto it: edges)
-        {
+        vector<vector<int>> adj[n+1];
+        
+        for(auto it : edges){
             adj[it[0]].push_back({it[1], it[2]});
             adj[it[1]].push_back({it[0], it[2]});
         }
         
-        vector<int>dist(n+1, 1e9), parent(n+1);
+        vector<int> dist(n+1, 1e9), parent(n+1);
         for(int i=1; i<=n; i++) parent[i] = i;
         
         dist[1] = 0;
         
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> >pq;
         pq.push({0, 1});
         
-        while(!pq.empty())
-        {
-            int dis = pq.top().first;
-            int node = pq.top().second;
+        while(!pq.empty()){
+            auto frontNode = pq.top();
             pq.pop();
             
-            for(auto it: adj[node])
-            {
-                int adjNode = it.first;
-                int edW = it.second;
+            int dis = frontNode.first;
+            int node = frontNode.second;
+            
+            for(auto it : adj[node]){
+                int adjnode = it[0];
+                int edgewt = it[1];
                 
-                if(edW + dis < dist[adjNode])
-                {
-                    dist[adjNode] = edW + dis;
-                    pq.push({dist[adjNode], adjNode});
-                    parent[adjNode] = node;
+                if(dis + edgewt < dist[adjnode]){
+                    dist[adjnode] = dis + edgewt;
+                    pq.push({dist[adjnode], adjnode});
+                    parent[adjnode] = node;
                 }
             }
         }
+        
         if(dist[n] == 1e9) return {-1};
         
-        vector<int>path;
+        vector<int> path;
         int node = n;
-        while(parent[node] != node)
-        {
+        while(parent[node] != node){
             path.push_back(node);
             node = parent[node];
         }
