2041bb84c3dac20ba3432c678404069c0d826d56
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-09-17 15:50:15 +0530

    Added solution - LeetHub

--- Path With Minimum Effort - GFG/path-with-minimum-effort.cpp
+++ Path With Minimum Effort - GFG/path-with-minimum-effort.cpp
@@ -6,39 +6,46 @@ using namespace std;
 class Solution {
   public:
     int MinimumEffort(vector<vector<int>>& heights) {
-        priority_queue< pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>,
-        greater<pair<int,pair<int,int>>>>pq;
-        int n= heights.size(), m = heights[0].size();
-        vector<vector<int>>dist(n, vector<int>(m, 1e9));
-        int dr[] = {-1, 0, 1, 0};
-        int dc[] = {0, 1, 0, -1};
-        pq.push({0, {0, 0}});
-        while(!pq.empty())
-        {
-            auto it = pq.top(); pq.pop();
-            int diff = it.first;
-            int r = it.second.first;
-            int c = it.second.second;
-            
-            if(r == n-1 && c == m-1) return diff;
-            for(int i=0; i<4; i++)
-            {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                
-                if(nr >=0 && nr < n && nc >=0 && nc <m)
-                {
-                    int effort = abs(heights[nr][nc] - heights[r][c]);
-                    int newdiff = max(effort, diff);
-                    if(newdiff < dist[nr][nc])
-                    {
-                       dist[nr][nc] = newdiff;
-                       pq.push({newdiff, {nr, nc}});
-                    }
+        
+        int n = heights.size();
+    int m = heights[0].size();
+
+    vector<vector<int>> dist(n, vector<int>(m, 1e9));
+    dist[0][0] = 0;
+
+    int delR[4] = {-1, 0, 1, 0};
+    int delC[4] = {0, 1, 0, -1};
+
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+    pq.push({0, {0, 0}});
+
+    while (!pq.empty()) {
+        auto frontNode = pq.top();
+        pq.pop();
+
+        int dis = frontNode.first;
+        int x = frontNode.second.first;
+        int y = frontNode.second.second;
+
+        if (x == n - 1 && y == m - 1) return dis;
+
+        for (int i = 0; i < 4; i++) {
+            int xc = x + delR[i];
+            int yc = y + delC[i];
+
+            if (xc >= 0 && xc < n && yc >= 0 && yc < m) {
+                int absdiff = abs(heights[xc][yc] - heights[x][y]);
+
+                int maxDiff = max(absdiff, dist[x][y]);
+
+                if (maxDiff < dist[xc][yc]) {
+                    dist[xc][yc] = maxDiff;
+                    pq.push({maxDiff, {xc, yc}});
                 }
             }
         }
-        return 0;
+    }
+    return -1;
     }
 };
 
