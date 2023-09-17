f71dc2f05346c5dbf27e1a38245f63c11ffd460a
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-09-17 23:51:41 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,101 +3,36 @@
 using namespace std;
 
 // } Driver Code Ends
-
-class DJS{
-    private:
-        vector<int>rank, parent, size;
-    public:
-        //constructor
-        DJS(int n)
-        {
-            rank.resize(n+1, 0);
-            parent.resize(n+1);
-            size.resize(n+1, 1);
-            for(int i=0; i<=n; i++)
-                parent[i] = i;
-        }
-
-        //finding the ultimate parent of the node
-        int findUltPar(int node)
-        {
-            if(node == parent[node]) return node;
-            //otherwise call the function recursively
-            return parent[node] = findUltPar(parent[node]);
-        }
-
-        void UnionByRank(int u, int v){
-            int UP_u = findUltPar(u);
-            int UP_v = findUltPar(v);
-            if(UP_u == UP_v) return;
-
-            if(rank[UP_u] < rank[UP_v])
-            {
-                parent[UP_u] = UP_v;
-            }
-            else if(rank[UP_v] < rank[UP_u])
-            {
-                parent[UP_v] = UP_u;
-            }
-            //else the rank is same and hence
-            else{
-                parent[UP_v] = UP_u;
-                rank[UP_u]++;
-            }
-        }
-
-        //UniouBySize
-        void UnionBySize(int u, int v){
-            int UP_u = findUltPar(u);
-            int UP_v = findUltPar(v);
-            if(UP_u == UP_v) return;
-
-            if(size[UP_u] < size[UP_v])
-            {
-                parent[UP_u] = UP_v;
-                size[UP_v] += size[UP_u];
-            }
-            else{
-                parent[UP_v] = UP_u;
-                size[UP_u] += size[UP_v];
-            }
-        }
-};
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        vector< pair<int, pair<int,int>>>edges;
-        for(int node = 0; node < V; node++)
+        vector<int>vis(V, 0);
+        priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        pq.push({0,0});
+        int sum = 0;
+        
+        while(!pq.empty())
         {
+            auto it = pq.top();
+            pq.pop();
+            int node = it.second;
+            int wt = it.first;
+            
+            if(vis[node]) continue;
+            vis[node] = 1;
+            sum += wt;
             for(auto it: adj[node])
             {
-                int adjnode = it[0];
-                int wt = it[1];
-                
-                edges.push_back({wt, {node, adjnode}});
+                if(!vis[it[0]])
+                {
+                    pq.push({it[1], it[0]});
+                }
             }
         }
-        
-        DJS ds(V);
-        sort(edges.begin(), edges.end());
-        int mstwt = 0;
-        
-        for(auto it: edges)
-        {
-            int wt = it.first;
-            int u = it.second.first;
-            int v = it.second.second;
-            
-            if(ds.findUltPar(u) != ds.findUltPar(v))
-            {
-                mstwt += wt;
-                ds.UnionBySize(u, v);
-            }
-        }
-        return mstwt;
+        return sum;
     }
 };
 
