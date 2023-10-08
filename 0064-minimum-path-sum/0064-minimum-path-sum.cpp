13eda36bc93e5df1b8590b66a9415a3cc52ac3fc
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-10-08 15:31:09 +0530

    Time: 3 ms (97.99%), Space: 10.4 MB (20.11%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -13,21 +13,45 @@ public:
         return dp[i][j] = min(l, r);
     }
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>>dp(grid.size(), vector<int>(grid[0].size(), -1));
-        return solve(grid, grid.size()-1, grid[0].size()-1, dp);
+        vector<vector<int>>dp(grid.size(), vector<int>(grid[0].size(), 0));
+        //return solve(grid, grid.size()-1, grid[0].size()-1, dp);
         
         m = grid.size(), n = grid[0].size();
-        for(int i=0; i<m; i++){
+        dp[0][0] = grid[0][0];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0 && j > 0) {
+                    dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+                } else if (i > 0) {
+                    dp[i][j] = grid[i][j] + dp[i-1][j];
+                } else if (j > 0) {
+                    dp[i][j] = grid[i][j] + dp[i][j-1];
+                }
+            }
+        }
+        
+        
+        /*
+        vector<int> prev(n, 0);
+        for(int i=0; i<n; i++) prev[i] = grid[0][i];
+        
+        for(int i=1; i<m; i++){
+            vector<int> temp(n, 0);
             for(int j=0; j<n; j++){
                 if(i==0 && j==0) dp[i][j] = grid[0][0];
                 else{
                     int l = 0, r = 0;
-                    if(j>0) l = grid[i][j] + dp[i][j-1];
-                    if(i>0) r = grid[i][j] + dp[i-1][j];
+                    if(j>0) l = grid[i][j] + temp[j-1];
+                    if(i>0) r = grid[i][j] + temp[j];
                     dp[i][j] = min(l, r);
                 }
             }
+            prev = temp;
         }
+        */
+        
+        
         return dp[m-1][n-1];
     }
 };
