43cb2c11832174d096e1d2b43acc12ac8e2b27b5
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-10-10 09:38:50 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -7,99 +7,50 @@ using namespace std;
 class Solution
 {
     public:
-    
-    int solveMem(int *weight, int* value, int index, 
-    int capacity, vector< vector<int> > &dp) // TLE
-{
-    if(index == 0)
-    {
-        if(weight[index] <= capacity) return value[0];
-        else return 0;
-    }
-    if(dp[index][capacity] != -1) return dp[index][capacity];
-    //incl
-    int incl = 0;
-    if(weight[index] <= capacity)
-        incl = value[index] + solveMem(weight, value, index-1, 
-                                      capacity-weight[index], dp);
-    //excl
-    int excl = 0 + solveMem(weight, value, index-1, capacity, dp);
-    return dp[index][capacity] = max(incl, excl);
-
-}
-    
-    
     //Function to return max value that can be put in knapsack of capacity W.
+    int solve(int w, int wt[], int val[], int n, int ind, vector<vector<int>> &dp){
+        if(ind == 0){
+            if(wt[ind] <= w) return val[ind];
+            return 0;
+        }
+        
+        if(dp[ind][w] != -1) return dp[ind][w];
+        
+        //take
+        int take = 0;
+        if(w >= wt[ind]) take = val[ind] + solve(w-wt[ind], wt, val, n, ind-1, dp);
+        
+        int leave = solve(w, wt, val, n, ind-1, dp);
+        
+        return dp[ind][w] = max(take, leave);
+        
+    }
+    
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-     /*  vector< vector<int> > dp(n, vector<int>(W+1, -1));
-    return solveMem(wt, val, n-1, W, dp);           */
-    
-    
-    
-     vector< vector<int> > dp(n, vector<int>(W+1, 0));
-     
-    //basecase
-    for(int i=wt[0]; i<=W; i++) dp[0][i] = val[0];
-    
-    for(int i=1; i<n; i++)
-    {
-        for(int wei=0; wei<=W; wei++)
-        {
-            int incl = 0;
-            if(wt[i] <= wei)
-                incl = val[i] + dp[i-1][wei - wt[i]];
-            //excl
-            int excl = 0 + dp[i-1][wei];
-            
-            //update
-            dp[i][wei] = max(incl, excl);
-        }
-    }
-        
-        return dp[n-1][W];                         
-        
-/*        vector<int>prev(W+1, 0), curr(W+1, 0);
-        for(int i=wt[0]; i<=W; i++) prev[i] = val[0];
-        
-    for(int i=1; i<n; i++)
-        {
-            for(int wei=0; wei<=W; wei++)
-            {
-                int incl = 0;
-                if(wt[i] <= wei)
-                    incl = val[i] + prev[wei - wt[i]];
-                //excl
-                int excl = 0 + prev[wei];
+       //vector<vector<int>> dp(1001, vector<int>(1001, 0));
+       //return solve(W, wt, val, n, n-1, dp);
+       
+       vector<int> prev(W+1, 0), curr(W+1, 0);
+       for(int weight=wt[0]; weight<=W; weight++){
+          prev[weight] = val[0];
+       }
+       
+       //changing parmaerters are ind and weight
+       for(int ind=1; ind<n; ind++){
+           for(int weight=0; weight<=W; weight++){
+                int take = 0;
+                if(weight >= wt[ind]) take = val[ind] + prev[weight-wt[ind]];
                 
-                //update
-                curr[wei] = max(incl, excl);
-            }
-            prev = curr;
-        }
-    
-    return prev[W];                 */
-    
-    //single row optimization
-    
-    //  vector<int>prev(W+1, 0);
-    //   for(int i=wt[0]; i<=W; i++) prev[i] = val[0];
-      
-    //      for(int i=1; i<n; i++)
-    //         {
-    //             for(int wei = W; wei>=0; wei--)
-    //             {
-    //                 int incl = 0;
-    //                 if(wt[i] <= wei)
-    //                     incl = val[i] + prev[wei - wt[i]];
-    //                 //excl
-    //                 int excl = 0 + prev[wei];
-                    
-    //                 //update
-    //                 prev[wei] = max(incl, excl);
-    //             }
-    //         }
-    //     return prev[W];
+                int leave = prev[weight];
+                
+                curr[weight] = max(take, leave);
+           }
+           prev = curr;
+       }
+       
+       
+       return prev[W];
     }
 };
 
