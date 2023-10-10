d3710c83364817691f0449491b9e3b8b3d15af13
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-10-10 10:22:27 +0530

    Time: 75 ms (20.60%), Space: 34.8 MB (14.38%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,47 +1,42 @@
-class Solution
-{
-    public:
-        vector<vector < int>> updateMatrix(vector<vector < int>> &g)
-        {
-            int n = g.size();
-            int m = g[0].size();
-            vector<vector < int>> vis(n, vector<int> (m, 0));
-            queue<pair<pair<int, int>, int>> q;
-
-            vector<vector < int>> ans(n, vector<int> (m, 0));
-            vector<int>dr = {-1, 0, 1, 0};
-            vector<int>dc = {0, 1, 0, -1};
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    if (g[i][j] == 0)
-                    {
-                        q.push({{i, j}, 0});
-                        vis[i][j] = 1;
-                    }
-                }
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
+        int m = grid.size();
+	    int n = grid[0].size();
+	    vector<vector<int>> ans(m, vector<int>(n, 0));
+	    vector<vector<int>> vis(m, vector<int>(n, 0));
+	    vector<int> drow = {-1,0,1,0};
+	    vector<int> dcol = {0,1,0,-1};
+	    
+	    queue<pair<pair<int,int>, int>>q;
+	    for(int i=0; i<m; i++){
+	        for(int j=0; j<n; j++){
+	            if(grid[i][j] == 0) q.push({{i, j}, 0});
+	        }
+	    }
+	    
+	    while (!q.empty()) {
+        auto frontNode = q.front();
+        q.pop();
+        auto cords = frontNode.first;
+        auto dist = frontNode.second;
+    
+        int x = cords.first, y = cords.second;
+    
+        for (int i = 0; i < 4; i++) {
+            int nx = x + drow[i];
+            int ny = y + dcol[i];
+    
+            // Check if (nx, ny) is valid and update distances
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n && vis[nx][ny] == 0 && grid[nx][ny] == 1) {
+                
+                ans[nx][ny] = dist+1;
+                vis[nx][ny] = 1;
+                q.push({{nx, ny}, dist+1});
             }
-
-            while (!q.empty())
-            {
-                int r = q.front().first.first;
-                int c = q.front().first.second;
-                int dis = q.front().second;
-                q.pop();
-
-                ans[r][c] = dis;
-                for (int i = 0; i < 4; i++)
-                {
-                    int nr = r + dr[i];
-                    int nc = c + dc[i];
-                    if (nr >= 0 && nr < n && nc >= 0 && nc < m && vis[nr][nc] == 0)
-                    {
-                        vis[nr][nc] = 1;
-                        q.push({{nr, nc}, dis+1});
-                    }
-                }
-            }
-            return ans;
         }
+    }
+
+	    return ans;
+    }
 };
