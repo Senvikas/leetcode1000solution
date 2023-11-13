3ca621ed47adf53d358a2968d76dd3b5cbfce615
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-11-13 19:06:24 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (73.55%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,30 +1,43 @@
 class Solution {
-
-    public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = new ArrayList<>();
-        int i = 0, sn = -1, en = matrix.length, j = 0, sm = -1, em = matrix[0].length;
-        while (isValid(i, j, sn, sm, en, em)) {
-            while (isValid(i, j, sn, sm, en, em)) res.add(matrix[i][j++]);
-            j--;
-            i++;
-            sn++;
-            while (isValid(i, j, sn, sm, en, em)) res.add(matrix[i++][j]);
-            i--;
-            j--;
-            em--;
-            while (isValid(i, j, sn, sm, en, em)) res.add(matrix[i][j--]);
-            j++;
-            i--;
-            en--;
-            while (isValid(i, j, sn, sm, en, em)) res.add(matrix[i--][j]);
-            i++;
-            j++;
-            sm++;
+    public List<Integer> spiralOrder(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        
+        int srow=0, scol=0, lrow=n-1, lcol=m-1;
+        int cnt = 0;
+        List<Integer> ans = new ArrayList<>();
+        while(cnt < m*n){
+            //if(cnt > m*n) break;
+            //first row -> traverse on columns
+            for(int j=scol; j<=lcol; j++){
+                ans.add(mat[srow][j]);
+                cnt++;
+            }
+            srow++; // 1
+            if(cnt >= m*n) break;
+            //last col -> t........ on rows
+            for(int i=srow; i<=lrow; i++){
+                ans.add(mat[i][lcol]);
+                cnt++;
+            }
+            lcol--;   // 3
+            if(cnt >= m*n) break;
+            //last row
+            for(int j=lcol; j>=scol; j--){
+                ans.add(mat[lrow][j]);
+                cnt++;
+            }
+            lrow--;  // 3
+            if(cnt >= m*n) break;
+            //first col
+            for(int i=lrow; i>=srow; i--){
+                ans.add(mat[i][scol]);
+                cnt++; 
+            }
+            scol++;  // 1
+            if(cnt >= m*n) break;
         }
-        return res;
-    }
-
-    private static boolean isValid(int i, int j, int sn, int sm, int en, int em) {
-        return (i > sn && j > sm && i < en && j < em);
+        
+        return ans;
     }
 }
