438490c9de809ee865b04e73f2a0a95a2fdaf8e4
Senvikas <111661736+Senvikas@users.noreply.github.com>
2023-12-18 22:20:50 +0530

    Time: 28 ms (15.97%), Space: 20.7 MB (28.86%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -1,11 +1,39 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-       // int ma1 = nums[0], ma2 = nums[1], mi1 = nums[2], mi2 = nums[3];
-        sort(nums.begin(), nums.end());
-        int first = nums[0]*nums[1];
-        int sec = nums[nums.size()-1]*nums[nums.size()-2];
+        int ma1 = nums[0], ma2 = nums[1], mi1 = nums[2], mi2 = nums[3];
+        vector<int> four = {ma1, ma2, mi1, mi2};
+        sort(four.begin(), four.end());
+        mi1 = four[0];
+        mi2 = four[1];
+        ma1 = four[3];
+        ma2 = four[2];
+    
+        for(int i=4; i<nums.size(); i++){
+            int ele = nums[i];
+            
+            // should max be updated
+            if(ele > ma2 || ele > ma1){
+                if(ele > ma1){
+                    ma2 = ma1;
+                    ma1 = ele;
+                }else
+                    ma2 = ele;
+            }
+            
+            //should min be updated
+            if(ele < mi2 || ele < mi1){
+                if(ele < mi1){
+                    mi2 = mi1;
+                    mi1 = ele;
+                }else
+                    mi2 = ele;
+            }
+            
+        }
         
+        int first = mi1 * mi2;
+        int sec = ma1 * ma2;
         return -( first - sec);
     }
 };
