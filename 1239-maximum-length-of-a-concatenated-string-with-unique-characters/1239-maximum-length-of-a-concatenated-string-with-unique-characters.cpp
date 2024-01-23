32d9e345f9d1412fe0a8aab094afedcb43f46110
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-01-24 02:09:05 +0530

    Time: 12 ms (79.02%), Space: 15.2 MB (68.27%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,29 +1,37 @@
-class Solution
-{
-    public:
-        int maxLength(vector<string> &arr)
-        {
+class Solution {
+public:
+    bool hasDup(string &s1, string &s2){
+        int arr[26]={0};
+        for(char &ch:s1){
+            if(arr[ch-'a']>0) return true;
 
-            int maxlen = 0;
-            int n = arr.size();
-            for(int i=0; i<(1<<n); i++){
-                vector<bool> vis(26, 0);
-                string s = "";
-                bool flag = true;
-                for(int j=0; j<n; j++){
-                    if(i & (1<<j)){
-                        for(char c : arr[j])
-                        {
-                            if(vis[c-'a']){flag = false; break;}
-                            vis[c-'a'] = 1;
-                        }
-                        if(flag==false) continue;
-                        s+=arr[j];
-                        maxlen = max(maxlen, (int)s.length());
-                    }
-                }
-            }
-
-            return maxlen;
+            arr[ch-'a']++;
         }
+        for(char &ch:s2){
+            if(arr[ch-'a']>0) return true;
+        }
+        return false;
+    }
+
+    int solve(int i, vector<string>&arr, string temp, int n){
+        if(i>=n) return temp.length();
+        int exclude=0;
+        int include=0;
+
+        if(hasDup(arr[i], temp)){
+            exclude=solve(i+1, arr, temp, n);
+        }
+        else{
+            include=solve(i+1, arr, temp+arr[i], n);
+            exclude=solve(i+1, arr, temp, n);
+        }
+        return max(include, exclude);
+    }
+
+    int maxLength(vector<string>& arr) {
+        string temp = "";
+        int n=arr.size();
+        int i=0;
+        return solve(i, arr, temp, n);
+    }
 };
