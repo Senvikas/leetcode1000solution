c66900212680fd497eadcaf2364867b2f0e3bdae
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-02-05 11:02:37 +0530

    Time: 4 ms (86.54%), Space: 17.3 MB (5.43%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,31 +1,62 @@
-class Solution
-{
-    public:
-        void dfs(vector<vector < int>> &arr, int x, int y, int n, int m, vector< int > &dx, vector< int > &dy, int st, int color)
-        {
-           	//    vis[x][y] = 1;
-            for (int i = 0; i < 4; i++)
+using pii = pair<int,int>;
+class Solution {
+public:
+    
+    //To check if the new cords are well withing the ranged of grid.
+    bool valid(int i, int j, vector<vector<int>> &grid){
+        int m = grid.size(), n = grid[0].size();
+        if(i>=0 && i<m && j>=0 && j<n) return true;
+        return false;
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int oldCol = image[sr][sc];
+        int m = image.size(), n = image[0].size();
+        
+        //Don't temper the given data, store in other grid and change in that one.
+        vector<vector<int>> clone = image;
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        
+        //To move to adjacent cords
+        vector<int>dx = {1, 0, 0, -1};
+        vector<int>dy = {0, -1, 1, 0};
+        
+        int cnt = 0;
+        queue<pii> q;
+        q.push({sr, sc});
+        vis[sr][sc] = 1;
+        
+        while(!q.empty()){
+            //Need to process all the elements of q at once.
+            int size = q.size();
+            for(int i=0; i<size; i++)
             {
-                int newx = x + dx[i];
-                int newy = y + dy[i];
-                if (newx >= 0 && newy >= 0 && newx < n && newy < m && arr[newx][newy] != color && arr[newx][newy] == st)
-                {
-                    arr[newx][newy] = color;
-                    dfs(arr, newx, newy, n, m, dx, dy, st, color);
+                auto frontNode = q.front();
+                q.pop();
+                
+                int sr = frontNode.first, sc = frontNode.second;
+                
+                //If current cell has an old col
+                if(clone[sr][sc] == oldCol){
+                    clone[sr][sc] = color;
+                    cnt++;
+
+                    for(int move=0; move<4; move++){
+                        //Finding adjacent cords to the current cords
+                        int srn = sr + dx[move];
+                        int scn = sc + dy[move];
+                        
+                        //Cords are valid and Not visited previously and having old color then store them to process next.
+                        if(valid(srn, scn, clone) && !vis[srn][scn] && clone[srn][scn] == oldCol){
+                            q.push({srn, scn});
+                            vis[srn][scn] = 1;
+                        }
+                    }
+                
                 }
             }
         }
-    vector<vector < int>> floodFill(vector<vector < int>> &image, int sr, int sc, int color)
-    {
-        int start = image[sr][sc];
-        int n = image.size();
-        int m = image[0].size();
-       	//        vector<vector<int>>vis(n,vector < int>(m, 0));
-        vector<int> dx = { -1, 0, 1, 0};
-        vector<int> dy = { 0, 1, 0, -1};
-
-        dfs(image, sr, sc, n, m, dx, dy, start, color);
-        image[sr][sc] = color;
-        return image;
+        //cout << "Number of Pixels that changed to new Colors: " << cnt << endl;
+        return clone;
     }
 };
