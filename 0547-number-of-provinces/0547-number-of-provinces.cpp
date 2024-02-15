52a9773ae0328e8bfe9a8bbd75a8d87cb6274186
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-02-15 16:09:27 +0530

    Time: 22 ms (62.10%), Space: 18.2 MB (34.07%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,43 +1,74 @@
-class Solution
-{
-    public:
-        void dfs(int *vis, vector<int> adj[], int node)
-        {
-            vis[node] = 1;
-            for (auto it: adj[node])
-            {
-                if (!vis[it])
-                    dfs(vis, adj, it);
-            }
+class DisjointSet {
+public:
+    vector<int> rank, parent, size; 
+public: 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
         }
+    }
 
-    int findCircleNum(vector<vector < int>> &isConnected)
-    {
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+}; 
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-       	//to change adj matrix to adj list
-        vector<int> adj[n];
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (isConnected[i][j] == 1 && i != j)
-                {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+        DisjointSet* ds = new DisjointSet(n);
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(isConnected[i][j] == 1){
+                    ds->unionByRank(i, j);
                 }
             }
         }
 
-        int vis[201] = { 0 };
-        int cnt = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (!vis[i])
-            {
-                cnt++;
-                dfs(vis, adj, i);
-            }
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            if(ds->parent[i] == i)
+                ans++;
         }
-        return cnt;
+
+        return ans;
     }
 };
