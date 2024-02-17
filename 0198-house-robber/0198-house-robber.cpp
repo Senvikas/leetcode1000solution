4a56fe961723474ff7d5af6281b972a1ff455370
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-02-18 02:13:30 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (40.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,35 +1,82 @@
-class Solution
-{
-    public:
-        int solve(int ind, vector<int> &arr, vector<int> &dp)
-        {
-            if (ind == 0) return arr[ind];
-            if (ind < 0) return 0;
-            if (dp[ind] != -1) return dp[ind];
-            int exc = solve(ind - 1, arr, dp);
-            int inc = arr[ind] + solve(ind - 2, arr, dp);
+    class mySolution{
+        public:
+        int spaceOpt(vector<int> &moneyInHouses){
+            int n = moneyInHouses.size();
 
-            return dp[ind] = max(inc, exc);
+            //base case
+            int next = moneyInHouses[n-1];
+            int next2 = 0;
+
+            for(int ind=n-2; ind>=0; ind--){
+                    int take = moneyInHouses[ind] + next2;                //next2 = dp[ind+2];
+                    int notTake = next;                                        //next = dp[ind+1];
+
+                    int curr = max(take, notTake);
+                    next2 = next;
+                    next = curr;
+            }
+
+            return next;
         }
 
-    int rob(vector<int> &arr)
-    {
-        int n = arr.size();
-       	//if (n == 2) return max(arr[0], arr[1]);
-        /*     vector<int> dp(n + 1, -1);
-                return solve(n - 1, arr, dp);              */
+        int Tabulation(vector<int> &moneyInHouses){
+            int n = moneyInHouses.size();
 
-       	//tabulation
-        vector<int> dp(n, 0);
-        dp[0] = arr[0];
-        if (n > 1)
-            dp[1] = max(arr[0], arr[1]);
-        for (int ind = 2; ind < n; ind++)
-        {
-            int exc = dp[ind - 1];
-            int inc = arr[ind] + dp[ind - 2];
-            dp[ind] = max(exc, inc);
+            vector<int> dp(n+1, 0);
+           //base case
+            dp[n] = 0;
+            dp[n-1] = moneyInHouses[n-1];
+
+            for(int ind=n-2; ind>=0; ind--){
+                    int take = moneyInHouses[ind] + dp[ind+2];
+                    int notTake = dp[ind+1];
+
+                    dp[ind] = max(take, notTake);
+            }
+
+            return dp[0];
         }
-        return dp[n - 1];
+
+        int solveRec(int ind, int n, vector<int> &moneyInHouses, vector<int> &dp){
+            //base case
+            if(ind == n-1) return moneyInHouses[ind];
+            if(ind >= n) return 0;
+
+            if(dp[ind] != -1) return dp[ind];    //2nd step
+
+            // I will do one part + Reamining Recursion
+            int take = moneyInHouses[ind] + solveRec(ind+2, n, moneyInHouses, dp);
+            int notTake = solveRec(ind+1, n, moneyInHouses, dp);
+
+            //return whatever max you can make
+            return dp[ind] = max(take, notTake);        //1st step
+        }
+
+        int houseRobber(vector<int> &moneyInHouses){
+            int n = moneyInHouses.size(); 
+            /*
+            vector<int> dp(n, -1);      //0th step
+            return solveRec(0, n, moneyInHouses, dp);
+            */
+
+           //Tabulation
+           //return Tabulation(moneyInHouses);
+
+           //space optimization
+           return spaceOpt(moneyInHouses);
+        }
+    };
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        mySolution obj;
+        // return obj.solveRec(0, n, nums, dp);
+        
+        // return obj.Tabulation(nums);
+        
+        return obj.spaceOpt(nums);
     }
 };
