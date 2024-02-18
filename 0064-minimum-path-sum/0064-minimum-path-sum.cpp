f91efc651c750d7c083d576c065278b996c7373c
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-02-18 19:06:25 +0530

    Time: 3 ms (96.74%), Space: 13 MB (7.55%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    int tabulation(vector<vector<int>> &grid){
+        int rows = grid.size(), cols = grid[0].size();
+        vector<vector<int>> dp(rows, vector<int>(cols, 0));
+        
+        //base case
+        dp[0][0] = grid[0][0];
+        
+        for(int row=1; row<rows; row++)
+            dp[row][0] += dp[row-1][0] + grid[row][0];
+        
+        for(int col=1; col<cols; col++)
+            dp[0][col] += dp[0][col-1] + grid[0][col];
+        
+        for(int row=1; row<rows; row++){
+            for(int col=1; col<cols; col++){
+                dp[row][col] = grid[row][col] + min(dp[row-1][col], dp[row][col-1]);
+            }
+        }
+        
+        return dp[rows-1][cols-1];
+    }
+    
     int solveRec(int row, int col, int rows, int cols, vector<vector<int>> &grid, vector<vector<int>> &dp){
         //base case
         if(row == rows-1 && col == cols-1) return grid[row][col];
@@ -20,6 +42,9 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, -1));     //0th step
-        return solveRec(0, 0, m, n, grid, dp);
+        //return solveRec(0, 0, m, n, grid, dp);
+        
+        
+        return tabulation(grid);
     }
 };
