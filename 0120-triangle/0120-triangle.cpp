d27b1d1a313dc94ebe2c1c7e1b9b6e03b09f0b6a
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-02-18 19:48:03 +0530

    Time: 6 ms (42.70%), Space: 11.8 MB (5.82%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    int tabulation(vector<vector<int>> &triangle){
+        int rows = triangle.size();
+        vector<vector<int>> dp(rows, vector<int>(rows, 0));
+
+        //base case -> if(i == rows-1) return dp[i][j] = tr[i][j];
+        for(int col=0; col<rows; col++){
+            dp[rows-1][col] = triangle[rows-1][col];
+        }
+
+        for(int row=rows-2; row>=0; row--){
+            for(int col=0; col<=row; col++){
+                int down = dp[row+1][col];
+                int diag = dp[row+1][col+1];
+
+                dp[row][col] = min(down, diag) + triangle[row][col];
+            }
+        }
+
+        return dp[0][0];
+
+    }
+    
     int rows;
     vector<vector<int>> dp;
     int solve(vector<vector<int>> &tr, int i, int j){
@@ -16,6 +38,8 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         rows = triangle.size();
         dp = vector<vector<int>>(rows, vector<int>(rows, -1));
-        return solve(triangle, 0, 0);
+        //return solve(triangle, 0, 0);
+        
+        return tabulation(triangle);
     }
 };
