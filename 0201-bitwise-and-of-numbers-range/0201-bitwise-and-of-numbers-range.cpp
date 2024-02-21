71f0ecc7c852616777ad430f0cf8e3081160564f
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-02-22 00:02:51 +0530

    Time: 4 ms (79.27%), Space: 8.8 MB (46.79%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+    // Find the common prefix
+    while (left < right) {
+        left >>= 1;
+        right >>= 1;
+        shift++;
+    }
+    // Shift back to obtain the result
+        cout << shift << endl;
+    return left << shift;
+        
+        
         int lastSetbit = 0;
         int n = left;
         while(n){
