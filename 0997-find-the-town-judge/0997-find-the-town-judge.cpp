5289ec80ba596fe05fc37d769698377e997d7016
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-02-23 00:16:36 +0530

    Time: 175 ms (18.93%), Space: 78.1 MB (7.59%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        set<int> st; vector<int>beleive_cnt(n+1, 0);
+        set<int> st;
         for(int i=1; i<=n; i++) st.insert(i);
-        for(auto it: trust)
-        {
-            int val = it[0];
-            if(st.find(val) != st.end()) st.erase(val);
-            beleive_cnt[it[1]]++;
+        
+        for(auto it: trust){
+            if(st.find(it[0]) != st.end()) st.erase(it[0]);
         }
+        
         cout << st.size() << endl;
         if(st.size() != 1) return -1;
-        int ans = 0;
-        for(auto i: st) ans = i;
-        if(beleive_cnt[ans] == n-1)
-        return ans;
+        int ans = (*st.begin());
+        
+        int believeCnt = 0;
+        for(auto it: trust){
+            if(it[1] == ans) believeCnt++;
+        }
+        if(believeCnt == n-1) return ans;
         return -1;
     }
 };
