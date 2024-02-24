bceae3f0f41749ae9886972406ebbdad62751bb5
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-02-24 06:20:51 +0530

    Time: 40 ms (25.04%), Space: 25.7 MB (22.61%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,50 +1,39 @@
-class Solution
-{
-    public:
-        int f(string s, string t, int ind1, int ind2, vector<vector < int>> &dp)
-        {
-            if (ind1 == 0 || ind2 == 0) return 0;
-
-            if (dp[ind1][ind2] != -1) return dp[ind1][ind2];
-            if (s[ind1 - 1] == t[ind2 - 1]) return dp[ind1][ind2] = 1 + f(s, t, ind1 - 1, ind2 - 1, dp);
-            else return dp[ind1][ind2] = max(f(s, t, ind1 - 1, ind2, dp), f(s, t, ind1, ind2 - 1, dp));
-           	//return dp[ind1][ind2];
+class Solution {
+public:
+    int solveRec(int ind1, int ind2, string &text1, string &text2, 
+                vector<vector<int>> &dp){
+        //base case
+        if(ind1 < 0 || ind2 < 0) return 0;
+        
+        //already computed
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        
+        //if matches
+        if(text1[ind1] == text2[ind2]){
+            return dp[ind1][ind2] = 1 + solveRec(ind1-1, ind2-1, text1, text2, dp);
         }
-
-    int longestCommonSubsequence(string s, string t)
-    {
-        int n = s.length(), m = t.length();
-
-       	//memoization    (must learn about shifting of the index in this question)      
-        /*      vector<vector < int>> dp(n + 1, vector<int> (m + 1, -1));
-                return f(s, t, n, m, dp); */
-
-       	//space opti
-        /*   vector<vector < int>> dp(n + 1, vector<int> (m + 1, 0));
-             for (int i = 1; i <= n; i++)
-             {
-                 for (int j = 1; j <= m; j++)
-                 {
-                     if (s[i - 1] == t[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
-                     else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                 }
-             }
-
-             return dp[n][m];          */
-        int k = max(n, m);
-        vector<int> prev(k + 1, 0), curr(k + 1, 0);
-
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-            {
-                if (s[i - 1] == t[j - 1]) curr[j] = 1 + prev[j - 1];
-                else curr[j] = max(prev[j], curr[j - 1]);
-            }
-            prev = curr;
-        }
-
-        int ans = prev[m];
+        //else, otherwise
+        return dp[ind1][ind2] = 0 + max(solveRec(ind1-1, ind2, text1, text2, dp), solveRec(ind1, ind2-1, text1, text2, dp));
+        
+    }
+    
+    
+    int longestCommonSubsequence(string text1, string text2) {
+        int ans = 0;
+        
+        int m = text1.size(), n = text2.size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        
+        ans = solveRec(m-1, n-1, text1, text2, dp);
+        
+        
+        
+        
+        
+        
+        
+        
+        
         return ans;
     }
 };
