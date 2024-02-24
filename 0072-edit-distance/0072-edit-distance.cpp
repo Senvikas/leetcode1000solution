5fa8eab6494ed06cdfab33c81a6ca016b27fffff
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-02-24 16:00:54 +0530

    Time: 13 ms (38.59%), Space: 12.2 MB (21.59%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,70 +1,31 @@
-class Solution
-{
-    public:
-        int solve(int i, int j, string &s, string &t, vector<vector < int>> &dp)
-        {
-            if (i == 0) return j;
-            if (j == 0) return i;
-            if (dp[i][j] != -1) return dp[i][j];
-            if (s[i - 1] == t[j - 1])
-                return dp[i][j] = solve(i - 1, j - 1, s, t, dp);
-
-            int insertion = 1 + solve(i, j - 1, s, t, dp);
-            int deletion = 1 + solve(i - 1, j, s, t, dp);
-            int replace = 1 + solve(i - 1, j - 1, s, t, dp);
-
-            return dp[i][j] = min(insertion, min(deletion, replace));
+class Solution {
+public:
+    int f(int i, int j, string& s, string& t, vector<vector<int>> &dp){
+        //base case  with ind shifting.
+        if(i==0 ){
+            return j;
         }
-    int minDistance(string s, string t)
-    {
-        int n = s.length(), m = t.length();
-        /*    vector<vector < int>> dp(n + 1, vector<int> (m + 1, -1));
-              return solve(n, m, s, t, dp);             */
-
-        /*      vector<vector < int>> dp(n + 1, vector<int> (m + 1, 0));
-               for (int i = 0; i <= n; i++)
-                   dp[i][0] = i;
-               for (int j = 0; j <= m; j++)
-                   dp[0][j] = j;
-
-               for (int i = 1; i <= n; i++)
-               {
-                   for (int j = 1; j <= m; j++)
-                   {
-                       if (s[i - 1] == t[j - 1])
-                           dp[i][j] = dp[i - 1][j - 1];
-                       else
-                       {
-                           int insertion = 1 + dp[i][j - 1];
-                           int deletion = 1 + dp[i - 1][j];
-                           int replace = 1 + dp[i - 1][j - 1];
-
-                           dp[i][j] = min(insertion, min(deletion, replace));
-                       }
-                   }
-               }
-               return dp[n][m];            */
-        vector<int> prev(m + 1, 0), curr(m + 1, 0);
-        for (int j = 0; j <= m; j++) prev[j] = j;
-
-        for (int i = 1; i <= n; i++)
-        {
-            curr[0] = i;
-            for (int j = 1; j <= m; j++)
-            {
-                if (s[i - 1] == t[j - 1])
-                    curr[j] = prev[j - 1];
-                else
-                {
-                    int insertion = 1 + curr[j - 1];
-                    int deletion = 1 + prev[j];
-                    int replace = 1 + prev[j - 1];
-
-                    curr[j] = min(insertion, min(deletion, replace));
-                }
-            }
-            prev = curr;
+        if(j==0) {
+            return i;
         }
-        return prev[m];
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        
+        if(s[i-1] == t[j-1]) return f(i-1, j-1, s, t, dp);
+        int del = 0, ins = 0, rep = 0;
+        del = 1 + f(i-1, j, s, t, dp);
+        ins = 1 + f(i, j-1, s, t, dp);
+        rep = 1 + f(i-1, j-1, s, t, dp);
+        
+        
+        return dp[i][j] = min(del, min(ins, rep));
+        
+        
+    }
+    int minDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        return f(m, n, word1, word2, dp);
     }
 };
