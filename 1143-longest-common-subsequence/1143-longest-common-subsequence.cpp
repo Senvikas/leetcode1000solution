34441585c700ce3ece6cc3a529dc784c1e5838d0
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-02-24 06:35:47 +0530

    Time: 44 ms (20.11%), Space: 25.8 MB (13.21%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,13 +3,14 @@ public:
     int solveRec(int ind1, int ind2, string &text1, string &text2, 
                 vector<vector<int>> &dp){
         //base case
-        if(ind1 < 0 || ind2 < 0) return 0;
+        // if(ind1 < 0 || ind2 < 0) return 0;
+        if(ind1 == 0 || ind2 == 0) return 0;
         
         //already computed
         if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
         
         //if matches
-        if(text1[ind1] == text2[ind2]){
+        if(text1[ind1-1] == text2[ind2-1]){  //for shifting added -1
             return dp[ind1][ind2] = 1 + solveRec(ind1-1, ind2-1, text1, text2, dp);
         }
         //else, otherwise
@@ -22,11 +23,11 @@ public:
         int ans = 0;
         
         int m = text1.size(), n = text2.size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
         
-        ans = solveRec(m-1, n-1, text1, text2, dp);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        ans = solveRec(m, n, text1, text2, dp);
         
-        
+        //vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
         
         
         
