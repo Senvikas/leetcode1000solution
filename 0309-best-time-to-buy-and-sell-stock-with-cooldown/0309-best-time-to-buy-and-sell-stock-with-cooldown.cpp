eafd676a6b58aac1e79fcb27108d7d3275989032
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-02-25 10:09:50 +0530

    Time: 0 ms (100.00%), Space: 14.1 MB (33.38%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,17 +1,30 @@
-class Solution {
-public:
-    int n;
-    int f(int ind, int cb, vector<int> &pr, vector<vector<int>> &dp){
-        if(ind >= n) return 0;
-        
-        if(dp[ind][cb] != -1) return dp[ind][cb];
-        if(cb) return dp[ind][cb] = max(-pr[ind] + f(ind+1, 0, pr, dp), 0 + f(ind+1, 1, pr, dp));
-        return dp[ind][cb] = max(pr[ind] + f(ind+2, 1, pr, dp), 0 + f(ind+1, 0, pr, dp));
+class Solution
+{
+    public:
+        int n;
+    int f(int ind, int cb, vector<int> &pr, vector<vector< int>> &dp)
+    {
+        if (ind >= n) return 0;
+
+        if (dp[ind][cb] != -1) return dp[ind][cb];
+        if (cb) return dp[ind][cb] = max(-pr[ind] + f(ind + 1, 0, pr, dp), 0 + f(ind + 1, 1, pr, dp));
+        return dp[ind][cb] = max(pr[ind] + f(ind + 2, 1, pr, dp), 0 + f(ind + 1, 0, pr, dp));
     }
-    
-    int maxProfit(vector<int>& prices) {
+
+    int maxProfit(vector<int> &prices)
+    {
         n = prices.size();
-        vector<vector<int>> dp(n+1, vector<int>(2, -1));
-        return f(0, 1, prices, dp);
+        vector<vector < int>> dp(n + 2, vector<int> (2, 0));
+       	//return f(0, 1, prices, dp);
+
+        for (int ind = n - 1; ind >= 0; ind--)
+        {
+                // removed cb cond, when cb = 1, first will execute and when 0 second.
+                dp[ind][1] = max(-prices[ind] + dp[ind + 1][0], 0 + dp[ind + 1][1]);
+            
+                dp[ind][0] = max(prices[ind] + dp[ind + 2][1], 0 + dp[ind + 1][0]);
+        }
+
+        return dp[0][1];
     }
 };
