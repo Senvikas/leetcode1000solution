d70de5f77a4617f1421a16334e3be168d14d068d
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-02-25 10:02:35 +0530

    Time: 4 ms (57.37%), Space: 14.3 MB (25.97%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,81 +1,17 @@
-class Solution
-{
-    public:
-        int solveMemo(vector<int> &prices, int day, bool buy, vector<vector< int>> &dp)
-        {
-
-            if (day >= prices.size())
-            {
-                return 0;
-            }
-            if (dp[day][buy] != -1) return dp[day][buy];
-
-           	// First Choice
-            int ans1 = solveMemo(prices, day + 1, buy, dp);	// no transaction this day
-
-           	// Second Choice
-            int ans2 = 0;
-           	// doing the required transaction this day
-            if (buy)
-            {
-                ans2 = -prices[day] + solveMemo(prices, day + 1, false, dp);
-            }
-            else
-            {
-                ans2 = prices[day] + solveMemo(prices, day + 2, true, dp);
-            }
-
-            return dp[day][buy] = max(ans1, ans2);
-        }
-    int solveDP1(vector<int> &prices)
-    {
-
-        int dp[prices.size() + 2][2];
-
-        for (int day = (int) prices.size() + 1; day >= 0; day--)
-        {
-            for (int buy = 0; buy <= 1; buy++)
-            {
-
-                int &ans = dp[day][buy];
-
-                if (day >= prices.size())
-                {
-                    ans = 0;
-                }
-                else
-                {
-
-                   	// First Choice
-                    int ans1 = dp[day + 1][buy];	// no transaction this say
-
-                   	// Second Choice
-                    int ans2 = 0;	// doing the required transaction this day
-
-                    if (buy)
-                    {
-                        ans2 = -prices[day] + dp[day + 1][0];
-                    }
-                    else
-                    {
-                        ans2 = prices[day] + dp[day + 2][1];
-                    }
-
-                    ans = max(ans1, ans2);
-                }
-            }
-        }
-
-        return dp[0][1];
+class Solution {
+public:
+    int n;
+    int f(int ind, int cb, vector<int> &pr, vector<vector<int>> &dp){
+        if(ind >= n) return 0;
+        
+        if(dp[ind][cb] != -1) return dp[ind][cb];
+        if(cb) return dp[ind][cb] = max(-pr[ind] + f(ind+1, 0, pr, dp), 0 + f(ind+1, 1, pr, dp));
+        return dp[ind][cb] = max(pr[ind] + f(ind+2, 1, pr, dp), 0 + f(ind+1, 0, pr, dp));
     }
-
-    int maxProfit(vector<int> &prices)
-    {
-       	// Memoization Solution
-        /*  vector<vector < int>> dp(prices.size() + 2, vector < int>(2, -1));
-           int ans = solveMemo(prices, 0, true, dp);
-           return ans;        */
-
-        return solveDP1(prices);
+    
+    int maxProfit(vector<int>& prices) {
+        n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, -1));
+        return f(0, 1, prices, dp);
     }
 };
