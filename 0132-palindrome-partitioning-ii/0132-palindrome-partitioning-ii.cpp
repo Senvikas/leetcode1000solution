c51f221ff15addea9e16f1ab482c20fe104e80ae
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-02-26 18:02:13 +0530

    Time: 1514 ms (38.71%), Space: 7.7 MB (66.50%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -24,7 +24,21 @@ public:
     }
     int minCut(string &s) {
         int n = s.size();
-        vector<int> dp(n, -1);
-        return f(0, n, s, dp)-1;
+        vector<int> dp(n+1, 0);
+        
+        for(int i=n-1; i>=0; i--){
+            int minCost = INT_MAX;
+            //string tmp = ""  -> i....j
+            
+            for(int j=i; j<n; j++){
+                if(isPal(i, j, s)){
+                    int cost = 1 + dp[j+1];
+                    minCost = min(minCost, cost);
+                }
+            }
+            dp[i] = minCost;
+        }
+        
+        return dp[0]-1;
     }
 };
