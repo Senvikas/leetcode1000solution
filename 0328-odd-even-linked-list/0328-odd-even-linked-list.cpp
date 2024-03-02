bf43b2eac814aa21d3a67d683e7b255838523d6b
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-03-02 18:38:46 +0530

    Time: 6 ms (79.06%), Space: 14 MB (30.24%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -1,70 +1,24 @@
-/**
- *Definition for singly-linked list.
- *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
- */
-/**
- *Definition for singly-linked list.
- *struct ListNode {
- *   int val;
- *   ListNode * next;
- *   ListNode() : val(0), next(nullptr) {}
- *   ListNode(int x) : val(x), next(nullptr) {}
- *   ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
- */
 class Solution
 {
     public:
         ListNode* oddEvenList(ListNode *head)
         {
-            if (!head) return NULL;
-            //here oh= oddhead, ot = oddtail, eh = evenhead, et = eventail
-            ListNode *oh = NULL, *ot = NULL, *eh = NULL, *et = NULL;
-            int cnt = 1;
-            while (head)
+            if (!head || !head->next) return head;
+
+            ListNode *odd = head;
+            ListNode *even = head->next;
+            ListNode *evenHead = even;
+
+            while (even && even->next)
             {
-                if (cnt % 2 == 0)
-                {
-                    if (eh == NULL)
-                    {
-                        eh = head;
-                        et = head;
-                        head = head->next;
-                    }
-                    else
-                    {
-                        et->next = head;
-                        et = et->next;
-                        head = head->next;
-                    }
-                }
-                else
-                {
-                    if (oh == NULL)
-                    {
-                        oh = head;
-                        ot = head;
-                        head = head->next;
-                    }
-                    else
-                    {
-                        ot->next = head;
-                        ot = ot->next;
-                        head = head->next;
-                    }
-                }
-                cnt++;
+                odd->next = even->next;
+                odd = odd->next;
+                even->next = odd->next;
+                even = even->next;
             }
-            if (oh == NULL) return eh;
-            if (eh == NULL) return oh;
-            et->next = NULL;
-            ot->next = eh;
-            return oh;
+
+            odd->next = evenHead;
+
+            return head;
         }
 };
