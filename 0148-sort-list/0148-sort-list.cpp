50ede085bb3787fe586da9389c93674d2f13d2b9
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-03-03 13:33:23 +0530

    Time: 145 ms (36.73%), Space: 73.6 MB (31.52%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,21 +10,52 @@
  */
 class Solution {
 public:
+    ListNode* findMid(ListNode* head){
+        auto s = head;
+        auto f = head->next;
+        
+        while(f && f->next){
+            s = s->next;
+            f = f->next->next;
+        }
+        
+        return s;
+    }
+    
+    ListNode* merge(ListNode* l1, ListNode* l2){
+        ListNode* dummy = new ListNode(-1);
+        auto tmp = dummy;
+        
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                tmp->next = l1;
+                tmp = l1;
+                l1 = l1->next;
+            }else{
+                tmp->next = l2;
+                tmp = l2;
+                l2 = l2->next;
+            }
+        }
+        
+        if(l1) tmp->next = l1;
+        else tmp->next = l2;
+        
+        
+        return dummy->next;
+    }
     ListNode* sortList(ListNode* head) {
-        vector<int> st;
-        auto tmp = head;
+        if(!head || !head->next) return head;
         
-        while(tmp){
-            st.push_back(tmp->val); tmp = tmp->next;
-        }
-        sort(st.begin(), st.end());
+        auto mid = findMid(head);
         
-        tmp = head;
-        for(auto ele: st){
-            tmp->val = ele;
-            tmp = tmp->next;
-        }
+        auto left = head;
+        auto right = mid->next;
+        mid->next = nullptr;
         
-        return head;
+        auto leftSorted = sortList(left);
+        auto rightSorted = sortList(right);
+        
+        return merge(leftSorted, rightSorted);
     }
 };
