65a217c78f55c068c4c001d443e69e4833b7f273
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-03-21 20:56:16 +0530

    Time: 24 ms (16.90%), Space: 11.8 MB (6.68%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,31 +1,54 @@
 class Solution
 {
     public:
-        ListNode* iterative(ListNode *head)
+        ListNode* recursive(ListNode *head)
         {
-            auto tmp = head;
-            ListNode *prev = nullptr;
+            if (!head || !head->next) return head;
 
-            while (tmp)
+           	//I'll do one part, rest recursion will take care.
+
+           	//get the remaining list reversed by recursion
+            auto reversed = recursive(head->next);
+
+           	//traverse to end of the reversed list and attach the current node(my work)
+            auto tmp = reversed;
+            while (tmp->next)
             {
-               	//store the list from this node onwards
-                auto next = tmp->next;
-
-               	//reverse untill this node by pointing this node's next to prev
-                tmp->next = prev;
-
-               	//update the prev as in the next iteration new prev would be tmp or prev->next;
-                prev = tmp;
-
-               	//move tmp to the next node(backup)
-                tmp = next;
+                tmp = tmp->next;
             }
+            tmp->next = head;	//curr;
+           	// since head is become the last node it should point to the nullptr
+            head->next = nullptr;
 
-            return prev;
+           	//return the reversed(head) as this contains the list in the reversed order
+            return reversed;
         }
+
+    ListNode* iterative(ListNode *head)
+    {
+        auto tmp = head;
+        ListNode *prev = nullptr;
+
+        while (tmp)
+        {
+           	//store the list from this node onwards
+            auto next = tmp->next;
+
+           	//reverse untill this node by pointing this node's next to prev
+            tmp->next = prev;
+
+           	//update the prev as in the next iteration new prev would be tmp or prev->next;
+            prev = tmp;
+
+           	//move tmp to the next node(backup)
+            tmp = next;
+        }
+
+        return prev;
+    }
 
     ListNode* reverseList(ListNode *head)
     {
-        return iterative(head);
+        return recursive(head);
     }
 };
