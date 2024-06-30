6bc0c34b70f5f5aa7c896c7ed5103bbac91db707
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-06-30 23:08:10 +0530

    Time: 11 ms (12.24%), Space: 16.1 MB (13.33%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,26 @@
-#include <vector>
-#include <algorithm>
 class Solution {
 public:
-    int binSearch(vector<int> &nums, int t, int s, int e){
-        if(s > e) return -1;
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans = {-1, -1};
+        int n = nums.size();
+        int s = 0, e = n-1;
         
-        int mid = s + (e-s)/2;
-        if(nums[mid] == t) return mid;
-        if(nums[mid] > t) return binSearch(nums, t, s, mid-1);
-        return binSearch(nums, t, mid+1, e);
-        
-    }
-    
-
-    vector<int> solve(vector<int>& nums, int target) {
-        int ind = binSearch(nums, target, 0, nums.size()-1);
-        if(ind == -1) return {-1, -1};
-        vector<int> ans;
-        
-        int i = ind;
-        while(i >= 0 && nums[i] == target){
-            i--;
+        while(s <= e){
+            int mid = s + (e-s)/2;
+            
+            if(nums[mid] == target){
+                int m = mid;
+                while(m-1 >= 0 && nums[m-1] == target) m--;
+                ans[0] = (m);
+                m = mid;
+                while(m+1 < n && nums[m+1] == target) m++;
+                ans[1] = (m);
+                return ans;
+            }
+            else if(nums[mid] < target) s = mid+1;
+            else e = mid-1;
         }
-        ans.push_back(i+1);
-        while(ind < nums.size() && nums[ind] == target){
-            ind++;
-        }
-        ans.push_back(ind-1);
         
         return ans;
     }
-    
-    vector<int> searchRange(vector<int>& nums, int target) {
-        //solve(nums, target);
-        
-        auto left = lower_bound(nums.begin(), nums.end(), target);
-        auto right = upper_bound(nums.begin(), nums.end(), target);
-        
-        if (left == right) {
-            return {-1, -1};
-        }
-        
-        int first = left - nums.begin();
-        int last = right - nums.begin()-1;
-        
-        return {first, last};
-    }
-    
 };
