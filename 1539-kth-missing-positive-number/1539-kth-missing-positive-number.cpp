078747f6655ebace3453cb8352a5e1bd081f8b8d
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-07-05 10:25:21 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (13.57%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,19 +1,20 @@
-class Solution
-{
-    public:
-        int findKthPositive(vector<int> &arr, int k)
-        {
-            k--;
-            vector<int> nums(2001, 0);	// taking 2000 for the last case possible (arr[i]=1000 &k=1000)
-            for (auto i: arr) nums[i] = 1;
-            for (int i = 1; i < 2001; ++i)
-            {
-                if (nums[i] == 0)
-                {
-                    if (k) k--;
-                    else return i;	// check for the first missing number after k-1 missing numbers
-                }
-            }
-            return -1;
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int e = arr.back();
+        int n = arr.size();
+        int missNs = e - n;
+        if(k > missNs){
+            return e + k - missNs;
         }
+        e = n;
+        int s = 0;
+        while(s<e){
+            int mid = s + (e-s)/2;
+            int missing = arr[mid] - mid - 1;
+            if(missing < k) s = mid + 1;
+            else e = mid;
+        }
+        return s + k;
+    }
 };
