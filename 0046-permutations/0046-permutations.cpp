a8b34dbc55815103ff83e38367cdb26fa3c82731
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-08-11 10:54:58 +0530

    Time: 3 ms (53.03%), Space: 10.7 MB (13.61%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
-    void solve(int ind,vector<int>& arr, vector<vector<int>> &ans,vector<int>&tmp)
-    {
-        //base case
-        if(ind == arr.size())
-        {ans.push_back(tmp);
-         return ;
+     void getPermutations(vector<int> &nums, vector<vector<int>> &ans, vector<int> perm, int ind, vector<int> &mapping){
+        if(ind == nums.size()){
+            ans.push_back(perm);
+            return;
         }
-        for(int i=ind; i<arr.size(); i++)
-        {
-            swap(arr[i], arr[ind]);
-            tmp.push_back(arr[ind]);
-            solve(ind+1, arr, ans, tmp);
-            tmp.pop_back();
-            swap(arr[i], arr[ind]);
+         
+         for (int i = 0; i < nums.size(); ++i) {
+            if (mapping[i] == 0) {
+                perm.push_back(nums[i]);
+                mapping[i] = 1;
+                getPermutations(nums, ans, perm, ind + 1, mapping);
+                perm.pop_back();
+                mapping[i] = 0;
+            }
         }
-    }
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> tmp;
-        solve(0, nums, ans, tmp);
-        return ans;
+         
+         
+         
+         return;
+     }
+    
+    void getPermutations(vector<int> &nums, vector<vector<int>> &ans, vector<int> perm, int ind){
+        if(ind == nums.size()){
+            ans.push_back(perm);
+            return;
+        }
         
+        for(int i=ind; i<nums.size(); i++){
+            swap(nums[i], nums[ind]);
+            perm.push_back(nums[ind]);
+            getPermutations(nums, ans, perm, ind+1);
+            swap(nums[i], nums[ind]);
+            perm.pop_back();
+        }
+        
+        return ;
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+         vector<vector<int>> ans;
+        vector<int> perm;
+        vector<int> mapping(nums.size(), 0);
+        
+        getPermutations(nums, ans, perm, 0, mapping);
+        
+        return ans;
     }
 };
