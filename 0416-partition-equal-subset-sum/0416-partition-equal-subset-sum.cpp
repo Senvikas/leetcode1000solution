3ac1dcff555bdfd504d7e70231c24e2966b09976
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-08-15 00:57:53 +0530

    Time: 28 ms (96.76%), Space: 98.9 MB (38.54%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,60 +1,34 @@
-class Solution
-{
-    public:
-        bool solve(vector<int> &arr, int s, int ind, vector<vector< int>> &dp)
-        {
-           	//base cases
+class Solution {
+public:
+        bool f(int ind, int t, vector<int> &arr, vector<vector<int>> &dp){
+        if(t == 0) return true;
+        if(t<0) return false;
+        if(ind == 0) return t==arr[0];
 
-            if (s == 0) return 1;
-            if (ind == 0) return arr[0] == s;
-            if (dp[ind][s] != -1) return dp[ind][s];
-            bool exc = solve(arr, s, ind - 1, dp);
-            bool inc = false;
-            if (s >= arr[ind]) inc = solve(arr, s - arr[ind], ind - 1, dp);
-
-            return dp[ind][s] = exc | inc;
+        if(dp[ind][t] != -1){
+            bool ans = dp[ind][t] == 0 ? false : true;
+            return ans;
         }
 
-    bool canPartition(vector<int> &arr)
-    {
-        int t_sum = 0;
-        int n = arr.size();
-        for (auto &x: arr) t_sum += x;
-        //cout << t_sum << endl;
-        if (t_sum & 1) return false;
-        int k = t_sum / 2 + 1;
-        //cout << k << endl;
-        /*    vector<vector < int>> dp(201, vector < int>(k,-1));
-               return solve(arr, k-1, n - 1, dp);            */
 
-        /*     vector<vector < bool>> dp(201, vector<bool> (k, 0));
-               for (int i = 0; i < n; i++) dp[i][0] = true;
-               if (arr[0] <= k) dp[0][arr[0]] = true;
+        bool ans = f(ind-1, t-arr[ind], arr, dp) || f(ind-1, t, arr, dp);
+        if(ans == true){
+            dp[ind][t] = 1;
 
-               for (int ind = 1; ind < n; ind++)
-               {
-                   for (int tar = 1; tar < k; tar++)
-                   {
-                       bool exc = dp[ind - 1][tar];
-                       bool inc = false;
-                       if (tar >= arr[ind]) inc = dp[ind - 1][tar - arr[ind]];
+        }else{
+            dp[ind][t] = 0;
 
-                      	//update the dp
-                       dp[ind][tar] = exc || inc;
-                   }
-               }
-               return dp[n - 1][k - 1];           */
-
-       	//further space optimization
-        vector<bool> prev(k, false), curr(k, false);
-        prev[0] = true;
-        if (arr[0] <= k) prev[arr[0]] = true;
-        for (int ind = 1; ind < n; ind++)
-        {
-            for (int tar = 1; tar < k; tar++)
-                curr[tar] = prev[tar] | ((tar >= arr[ind]) ? prev[tar - arr[ind]] : false);
-            prev = curr;
         }
-        return prev[k - 1];
+        return ans;
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return false;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum & 1) return false;
+        
+        vector<vector<int>> dp(n, vector<int>(sum/2+1, -1));
+        return f(n-1, sum/2, nums, dp);
     }
 };
