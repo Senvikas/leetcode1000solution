90c47da9fa4aa4122fbdc3851b13ca036086afdd
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-08-14 20:06:25 +0530

    Time: 6 ms (21.02%), Space: 10.6 MB (5.33%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -13,8 +13,20 @@ public:
         return dp[m][n] = u+l;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& og) {
-    
-        vector<vector<int>> dp(og.size(), vector<int>(og[0].size(), -1));
-        return f(og, og.size()-1, og[0].size()-1, dp);
+        if(og[0][0] == 1) return 0;
+        vector<vector<int>> dp(og.size(), vector<int>(og[0].size(), 0));
+        //return f(og, og.size()-1, og[0].size()-1, dp);
+        dp[0][0] = 1;
+        for(int i=0; i<og.size(); i++){
+            for(int j=0; j<og[0].size(); j++){
+                if(i==0 && j==0) continue;
+                int u = 0, l = 0;
+                
+                if(i-1>=0) u = og[i][j] == 0 ? dp[i-1][j] : 0;
+                if(j-1>=0) l = og[i][j] == 0 ? dp[i][j-1] : 0;
+                dp[i][j] = u+l;
+            }
+        }
+        return dp[og.size()-1][og[0].size()-1];
     }
 };
