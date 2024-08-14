5c5245ec54e421e4df7c3454745f29d6ffd48a27
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-08-14 18:32:15 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (35.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-
+    int f(int m, int n, vector<vector<int>> &dp){
+        if(m==0 && n==0) return 1;
+        if(dp[m][n] != -1) return dp[m][n];
+        int u=0,l=0;
+        if(m-1>=0) u = f(m-1, n, dp);
+        if(n-1>=0) l = f(m, n-1, dp);
+        return dp[m][n] = u+l;
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        for(int i=0;i<m;i++){
-            dp[i][0]=1;
-        }
-        for(int j=0;j<n;j++){
-            dp[0][j]=1;
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]=dp[i][j-1]+dp[i-1][j];
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        //return f(m-1, n-1, dp);
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         return dp[m-1][n-1];
-
     }
 };
