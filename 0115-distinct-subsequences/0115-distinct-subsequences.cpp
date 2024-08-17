884ff3a46092c9c0f04ebb9a05748b8737835c2e
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-08-17 20:17:36 +0530

    Time: 17 ms (66.00%), Space: 19.9 MB (64.23%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,60 +1,19 @@
-class Solution
-{
-    public:
-        int solve(string s, string t, int i, int j, vector<vector < int>> dp)
-        {
-            if (j == 0) return 1;
-            if (i == 0) return 0;
-
-            if (dp[i][j] != -1) return dp[i][j];
-            if (s[i - 1] == t[j - 1]) return dp[i][j] = (solve(s, t, i - 1, j - 1, dp) + solve(s, t, i - 1, j, dp));
-            else return dp[i][j] = solve(s, t, i - 1, j, dp);
-        }
-
-    int numDistinct(string s, string t)
-    {
-        int n = s.length(), m = t.length();
-        /*   vector<vector < int>> dp(n+1, vector<int> (m+1, -1));
-               return solve(s, t, n, m, dp);       */
-
-       	//tabulation - >> use one base index
-        /*      vector<vector < double>> dp(n + 1, vector<double> (m + 1, 0));
-                for (int i = 0; i <= n; i++)
-                    dp[i][0] = 1;
-
-                for (int i = 1; i <= n; i++)
-                {
-                    for (int j = 1; j <= m; j++)
-                    {
-                        if (s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-                        else dp[i][j] = dp[i - 1][j];
-                    }
-                }
-                return (int)dp[n][m];                   */
-        /*     vector<double> prev(m + 1, 0), curr(m + 1, 0);
-                prev[0] = curr[0] = 1;
-
-                for (int i = 1; i <= n; i++)
-                {
-                    for (int j = 1; j <= m; j++)
-                    {
-                        if (s[i - 1] == t[j - 1]) curr[j] = prev[j - 1] + prev[j];
-                        else curr[j] = prev[j];
-                    }
-                    prev = curr;
-                }
-                return (int) curr[m];               */
-
-        vector<double> prev(m + 1, 0);
-        prev[0] = 1;
-
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = m; j >= 1; j--)
-            {
-                if (s[i - 1] == t[j - 1]) prev[j] = prev[j - 1] + prev[j];
+class Solution {
+public:
+    int mod = 1e9+7;
+    int numDistinct(string s, string t) {
+        int n1 = s.size(); int n2 =t.size();
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+        for(int i=0; i<=n1; i++) dp[i][0] = 1;
+        
+        for(int i=1; i<=n1; i++){
+            for(int j=1; j<=n2; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = (dp[i-1][j-1]%mod + dp[i-1][j]%mod)%mod;
+                }else 
+                    dp[i][j] = dp[i-1][j]%mod;
             }
         }
-        return (int) prev[m];
+        return dp[n1][n2];
     }
 };
