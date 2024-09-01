46fa96908f7a6b23ab01b7176fc811b642f34986
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-09-01 12:14:39 +0530

    Time: 114 ms (81.03%), Space: 75.1 MB (37.71%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int window_start = 0, window_end = 0, max_length = 1;
+        unordered_map<int,int> unique_fruits;
+
+        while(window_end < fruits.size()){
+            int current_fruit_type = fruits[window_end];
+           
+            unique_fruits[current_fruit_type]++;
+
+
+            while(unique_fruits.size() > 2){
+                int start_fruit_type = fruits[window_start];
+                unique_fruits[start_fruit_type]--;
+                if(unique_fruits[start_fruit_type] == 0) {
+                    unique_fruits.erase(start_fruit_type);
+                }
+                window_start++;
+            }
+
+            max_length = max(max_length, window_end - window_start + 1);
+            window_end++;
+        }
+
+        return max_length;
+    }
+};
