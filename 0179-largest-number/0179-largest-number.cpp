ef24b6b65a4fd36f168016109a7adb1393b58dcc
Senvikas <111661736+Senvikas@users.noreply.github.com>
2024-09-18 21:23:40 +0530

    Time: 0 ms (100%), Space: 17.4 MB (14.46%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,54 +1,27 @@
 class Solution {
 public:
-    static bool comp(string &s1, string &s2){
-
-        if (s1 == "0") return false;
-    if (s2 == "0") return true;
-        int i;
-        for( i=0; i<min(s1.size(), s2.size()); i++){
-            if(s1[i] == s2[i]) continue;
-            else return s1[i] > s2[i];
-        }
-        i--;
-        auto s1ps2 =  s1 + s2;
-        auto s2ps1 = s2 + s1;
-
-        if(s2ps1 > s1ps2) return false;
-        return true;
-        return s1.back() > s2.back();
+    static bool comp(string &s1, string &s2) {
+        return s1 + s2 > s2 + s1;
     }
-
-    bool allZeros(const vector<int>& nums) {
-    for (int num : nums) {
-        if (num != 0) return false;
-    }
-    return true;
-}
-
 
     string largestNumber(vector<int>& nums) {
-
-        if(allZeros(nums)) return "0";
-        string ans = "";
-
         vector<string> numsStr;
-
-        for(auto num: nums){
+        for (int num : nums) {
             numsStr.push_back(to_string(num));
         }
 
+        // Sort using the optimized comparator
         sort(numsStr.begin(), numsStr.end(), comp);
 
-        // for(auto num: numsStr) cout << num << " ";
-        // cout << endl;
-        // reverse(numsStr.begin(), numsStr.end());
+        // If the first string is "0", the entire result should be "0"
+        if (numsStr[0] == "0") return "0";
 
-        for(auto num: numsStr) ans += num;
-        ans.erase(0, ans.find_first_not_of('0'));
-        if(!ans.size()) {
-            // cout << " Hi " << endl;
-            ans = "0";
+        // Concatenate all sorted strings
+        string ans;
+        for (string &num : numsStr) {
+            ans += num;
         }
+
         return ans;
     }
 };
