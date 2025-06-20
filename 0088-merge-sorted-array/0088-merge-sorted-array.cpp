7e44ecb10feedee32589115220f8e192b32576a7
Senvikas <111661736+Senvikas@users.noreply.github.com>
2025-06-20 23:40:07 +0530

    Time: 0 ms (100%), Space: 12.2 MB (70.48%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,24 +1,19 @@
-class Solution
-{
-    public:
-        void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
-        {
-            int i = m - 1;
-            int j = n - 1;
-            int k = m + n - 1;
-           	// if(m==0) nums1 = nums2;
-           	// if(n==0) return;
-            while (i >= 0 && j >= 0)
-            {
-                if (nums1[i] > nums2[j])
-                    nums1[k--] = nums1[i--];
-                else if (nums2[j] > nums1[i])
-                    nums1[k--] = nums2[j--];
-                else nums1[k--] = nums1[i--];
-            }
-            while (i >= 0)
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
                 nums1[k--] = nums1[i--];
-            while (j >= 0)
+            } else {
                 nums1[k--] = nums2[j--];
+            }
         }
+        // If remaining elements in nums2, copy them
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+        // No need to handle remaining nums1 elements since they're already in
+        // place
+    }
 };
