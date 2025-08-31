95340365179372175d29d80a6f967df04f6c137c
Senvikas <111661736+Senvikas@users.noreply.github.com>
2025-09-01 00:02:47 +0530

    Time: 279 ms (68.55%), Space: 8.7 MB (87.82%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,47 +1,66 @@
 class Solution {
 public:
-    bool isSafe(vector<vector<char>> &board, int row, int col, int num){
-        //check for row and column
-        for(int i=0; i<9; i++){
-            if(board[row][i] == char(num+'0')) return false;
-            if(board[i][col] == char(num+'0')) return false;
+    bool isSafe(char num, int row, int col, vector<vector<char>>& board)
+    {
+        // row and col check
+        for(int i=0; i<9; i++)
+        {
+            if(board[row][i] == num || board[i][col] == num)
+                return false;
         }
-        //check for 3*3 grid
-        int sr = (row/3) * 3;
-        int sc = (col/3) * 3;
-        
-        for(int i=sr; i<sr+3; i++){
-            for(int j=sc; j<sc+3; j++){
-                if(board[i][j] == char(num+'0')) return false;
+
+        // 3*3 grid check
+        int srow = (row/3)*3, scol = (col/3)*3;
+        for(int i=srow; i<srow+3; i++)
+        {
+            for(int j=scol; j<scol+3; j++)
+            {
+                if(board[i][j] == num)
+                    return false;
             }
         }
+
         return true;
     }
-    bool solve(vector<vector<char>> &board, int row, int col){
-    if(row >= 9) return true;
-    if(col >= 9) return solve(board, row+1, 0); // Added return statement
-    
-    //if any cell is empty, fill; else skip
-    
-    if(board[row][col] == '.'){
-        //try filling out this cell with any number from 1-9
-        for(int num = 1; num < 10; num++){
-            if(isSafe(board, row, col, num)){
-                board[row][col] = char(num + '0');
-                if(solve(board, row, col+1)) return true;
-                board[row][col] = '.';
-            }
-        }
-    }
-    else{
-        return solve(board, row, col+1);
-    }
-        
-    return false;
-}
 
-    
+    bool solve(int row, int col, vector<vector<char>>& board)
+    {
+        // if all the cols are filled
+        if(col == 9)
+        {
+            return solve(row+1, 0, board);
+        }
+
+        // if all the rows are filled
+        if(row == 9)
+            return true;
+
+        // if empty cell
+        if(board[row][col] == '.')
+        {
+            for(char num = '1'; num <= '9'; num++)
+            {
+                bool safe = isSafe(num, row, col, board);
+                if(safe)
+                {
+                    board[row][col] = num;
+
+                    if(solve(row, col+1, board) == true)
+                        return true;
+
+                    board[row][col] = '.';
+                }
+            }
+
+            // khali tha and you tried all the numbers and sudoku is not filled
+            return false;
+        }
+
+        return solve(row, col + 1, board); // move to the next cell
+    }
+
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board, 0, 0);
+        solve(0, 0, board);
+        return;
     }
 };
