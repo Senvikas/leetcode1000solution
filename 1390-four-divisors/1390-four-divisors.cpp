fe8c52e8cdc6821b5cb1b043057e5e657db2755a
Senvikas <111661736+Senvikas@users.noreply.github.com>
2026-01-04 09:41:26 +0530

    Time: 7 ms (85.96%), Space: 16.4 MB (30.05%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,34 +1,57 @@
 class Solution {
 public:
-    void check(int num, int &ans)
+    bool isPrime(int num)
     {
-        int sum = num+1, cnt = 0;
-        for(int n=2; n<=(num+1)/2; n++)
+        if(num < 2)
+            return false;
+
+        for(int d=2; d*d <= num; d++)
         {
-            if(num%n == 0)
+            if(num%d == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    int DivOf4(int num)
+    {
+        // case 1: 1 + p^3, p belongs to primes
+        int root = round(pow(num, 1.0/3));
+        if(root*root*root == num && isPrime(root))
+        {
+            int sum = 1 + root + root*root + num;
+            return sum;
+        }
+
+        // case 2: 1 + p + q + pq, p,q belongs to primes
+        // find p
+        int p = -1;
+        for(int d=2; d*d <= num; d++)
+        {
+            if(num%d == 0)
             {
-                if(cnt == 2)
-                { // gotten 2 already, this is 3rd.
-                    return;
-                }
-                sum += n;
-                cnt++;
+                p = d;
+                break;
             }
         }
 
-        if(cnt != 2)
-            return;   // got less then 2 divs.
+        if(p == -1 || p*p == num)
+            return 0;  // no divs || square
 
-        ans += sum;
+        int q = num/p;
+        if(p*q == num && isPrime(p) && isPrime(q))
+            return 1 + p + q + num;
+
+        return 0;
     }
     int sumFourDivisors(vector<int>& nums) {
         int ans = 0;
 
         for(int num: nums)
-        {
-            check(num, ans);
-        }
+            ans += DivOf4(num);
 
         return ans;
+
     }
 };
