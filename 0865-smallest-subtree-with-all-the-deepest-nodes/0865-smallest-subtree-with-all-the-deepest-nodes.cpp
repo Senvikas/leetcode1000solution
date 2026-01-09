50571441190252d8785c52e85d33a388277fd4a7
Senvikas <111661736+Senvikas@users.noreply.github.com>
2026-01-09 08:45:41 +0530

    Time: 0 ms (100%), Space: 16.4 MB (27.77%) - LeetHub

---
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        return dfs(root).second;
+    }
+
+    // Returns {depth, node}
+    pair<int, TreeNode*> dfs(TreeNode* root) {
+        if (!root) return {0, nullptr};
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        if (left.first == right.first) {
+            return {left.first + 1, root};
+        } else if (left.first > right.first) {
+            return {left.first + 1, left.second};
+        } else {
+            return {right.first + 1, right.second};
+        }
+    }
+};
